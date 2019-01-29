8ada3268d2d61f9c041472b547cd28fca35d7f69
Robbie Jaeger <jaeger.rob@gmail.com>
2019-01-29 10:51:47 -0700

    Add file loader for image usage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-// Do not delete or rename this file
 // This is the JavaScript entry file - your code begins here
+// Do not delete or rename this file
 
+// Tell webpack to use a CSS file
 import './css/base.css';
+
+//  Tell webpack to use an image (link to it in index.html)
+import './images/turing-logo.png'
+
 
 console.log('This is the JavaScript entry file - your code begins here.');
