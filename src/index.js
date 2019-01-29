f8cfcb4ab4262e7dc2f5dc8ed792d598684b008e
Robbie Jaeger <jaeger.rob@gmail.com>
2019-01-28 20:54:50 -0700

    Add note in entry file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Do not delete or rename this file
+// This is the JavaScript entry file - your code begins here
+
 import './css/base.css';
 
 console.log('This is the JavaScript entry file - your code begins here.');
