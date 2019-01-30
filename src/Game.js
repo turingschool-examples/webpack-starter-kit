86cc50700787fb254d39c1de24d90234671bee9f
Rachael Drennan <rdrennan0@gmail.com>
2019-01-30 14:46:08 -0700

    Add to Game-test

--- src/Game.js
+++ src/Game.js
@@ -3,3 +3,5 @@ class Game{
     
   }
 }
+
+export default Game;
---
+++ test/Game-test.js
@@ -0,0 +1,17 @@
+import chai from 'chai';
+import Game from '../src/Game.js';
+import domUpdates from '../src/domUpdates.js';
+const expect = chai.expect;
+
+describe('Game', () =>{
+  it('Game can be an object', () =>{
+    const game = new Game();
+    expect(game).to.be.an('object');
+
+  })
+
+
+
+
+})
+
