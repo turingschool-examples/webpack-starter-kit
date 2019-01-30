f2ccc11701cd394f1d56ceed400fa5e0f7153529
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-01-29 17:27:28 -0700

    Webpack stuff

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,10 @@
 class Game {
   constructor(currentRound, activePlayer, roundWinner, gameWinner){
-
+    this.currentRound = currentRound,
+    this.activePlayer = activePlayer,
+    this.roundWinner = roundWinner,
+    this.gameWinner = gameWinner
   }
+
+  
 }
