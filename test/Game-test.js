00e2cb50bf05226011bd06bb65b4daf18431f954
Elizabeth Asbell <e.asbell8@gmail.com>
2019-01-30 16:30:28 -0700

    Delete to sample test and initialize beginning tests.

---
+++ test/Game-test.js
@@ -0,0 +1,25 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import Game from '../src/Game.js'
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+  it('should instantiate a new game', function() {
+    let game = new Game()
+    expect(game).to.be.an('object');
+  });
+
+  it('should gather players into players array and set current player', function() {
+    let game = new Game();
+    game.gatherPlayers('a', 'b', 'c');
+    
+    expect(game.players).to.deep.equal(['a', 'b', 'c']);
+    expect(game.currentPlayer).to.equal(game.players[0])
+  });
+});
+
+
