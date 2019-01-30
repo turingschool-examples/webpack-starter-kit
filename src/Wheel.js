e1c4288a939893d8d84656403a1e7110dd03121c
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-01-29 20:52:23 -0700

    Add connectivity to modules

--- src/Wheel.js
+++ src/Wheel.js
@@ -1,5 +1,5 @@
 class Wheel {
   constructor(value) {
-
+    
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@
 
 // Tell webpack to use a CSS file
 import './css/base.css'; 
+import Game from'./Game.js';
 
 //  Tell webpack to use an image (link to it in index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
 
+window.onload = config
+
+function config() {
+const wheelOfFortune = new Game(1, 'playerone');
+
+wheelOfFortune.startGame();
+}
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
