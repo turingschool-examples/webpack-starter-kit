3a3f6e1b800bd9c0c6c3aa99019d140226a4332f
Taylor Sperry <sperry.taylor@gmail.com>
2019-01-30 17:36:22 -0700

    Begin collecting player names

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file
-import Game from './Game.js';
-import Player from './Player.js';
-import Wheel from './Wheel.js';
-import Puzzle from './Puzzle.js';
 
+// import Game from './Game.js';
+// import Player from './Player.js';
+// import Wheel from './Wheel.js';
+// import Puzzle from './Puzzle.js';
 
 // Tell webpack to use a CSS file
-import './css/base.css';
+// import './css/base.css';
 
-//  Tell webpack to use an image (link to it in index.html)
-import './images/turing-logo.png'
+$('.start-button').click(playGame);
+
+const player1 = $('#player1').val();
+const player2 = $('#player2').val();
+const player3 = $('#player3').val();
+const players = [];
+
+function collectPlayers(player) {
+  players.push(player);
+}
+
+collectPlayers(player1);
+collectPlayers(player2);
+collectPlayers(player3);
+
+function playGame(event) {
+  event.preventDefault();
+  alert('hi');
+}
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
 
