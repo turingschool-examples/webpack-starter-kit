bcd523ea2a45ca1a577fcc9a0960b0695c69ac98
SiimonStark <justinpyktel@gmail.com>
2019-01-31 19:01:49 -0700

    create pop-up: Ans

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,9 +1,9 @@
-export default {
-//   displayHeight: function(height) {
-//     document.querySelector('#box-height-display').innerHTML = height;
-},
-displayQuestion: function(question) {
-$('article h3').click($('.popup-question').text('works'))
- }
+// export default {
+// //   displayHeight: function(height) {
+// //     document.querySelector('#box-height-display').innerHTML = height;
+// },
+// displayQuestion: function(question) {
+// $('article h3').click($('.popup-question').text('works'))
+//  }
   
-}
+// }
