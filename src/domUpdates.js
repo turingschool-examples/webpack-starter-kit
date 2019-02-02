b9a1ccca0f9dfb694da0e7b51f9a560acf872d32
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-01 23:00:37 -0700

    Add player name section

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -6,9 +6,9 @@ const domUpdates = {
   },
 
   displayPlayers(p1, p2, p3) {
-      $('.player-names-0').text(p1.name);
-      $('.player-names-1').text(p2.name);
-      $('.player-names-2').text(p3.name);
+      $('#player-names-0').text(p1.name);
+      $('#player-names-1').text(p2.name);
+      $('#player-names-2').text(p3.name);
   },
 
   displayCategory(roundPuzzle) {
