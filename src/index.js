1e71b6c826cfb027deb6038b278e4f74c94a0253
Rachael Drennan <rdrennan0@gmail.com>
2019-02-02 12:16:22 -0700

    event listeners for board

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import Game from './Game.js';
 import Player from './Player.js';
 import Round from './Round.js';
-import domUpdate from './domUpdates.js';
+import domUpdates from './domUpdates.js';
 import Clue from './Clue.js';
 import DailyDouble from './DailyDouble.js';
 import './css/base.css';
@@ -10,6 +10,16 @@ import $ from 'jquery';
 import './images/player1.svg';
 import './images/player2.svg';
 import './images/player3.svg';
+
+
+
+$('.box').on('click', (e) => {
+  console.log('testing1');
+  domUpdates.GameBoardListener(event); 
+  console.log('testing2');
+
+});
+
 
 
 $('.start-game').on('click', (e) => {
