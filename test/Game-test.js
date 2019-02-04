cdbe1254b2202adf194d197669d1346766e86422
David Cisneros <Dcisneros101@gmail.com>
2019-02-04 13:19:48 -0700

    Refactor current test for new standards

--- test/Game-test.js
+++ test/Game-test.js
@@ -19,16 +19,32 @@ describe('Game', function() {
     let game = new Game();
     game.gatherPlayers('a', 'b', 'c');
     
-    expect(game.players).to.deep.equal(['a', 'b', 'c']);
-    expect(game.currentPlayer).to.equal(game.players[0])
+    expect(game.players).to.deep.equal([{name:'a', score: 0}, {name:'b', score: 0}, {name:'c', score: 0}]);
+    expect(game.currentPlayer).to.equal(game.players[0]);
+    expect(domUpdates.displayPlayerScore).to.be.called(1);
   });
 
-  it('should call upon domUpdates.toggleSplash when game.quitGame is called', function() {
+  it('should call upon toggleSplash when quitGame is called & set clues to an empty array', function() {
     let game = new Game();
 
     game.quitGame();
 
     expect(domUpdates.toggleSplash).to.be.called();
+    expect(game.cluesThisRound).to.deep.equal([]);
+  });
+
+  it('should switch between players', function() {
+    let game = new Game();
+    game.gatherPlayers('bob', 'jim', 'tom');
+  
+    game.switchPlayer(game.currentPlayer);
+    expect(game.currentPlayer).to.deep.equal(game.players[1]);
+
+    game.switchPlayer(game.currentPlayer);
+    expect(game.currentPlayer).to.deep.equal(game.players[2]);
+
+    game.switchPlayer(game.currentPlayer);
+    expect(game.currentPlayer).to.deep.equal(game.players[0]);
   });
 });
 
