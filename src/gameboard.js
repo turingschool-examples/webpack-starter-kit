a412bf7ba55d0207551c614d020a4ea711445d2f
Mason France <masonrf1@gmail.com>
2019-02-05 12:04:37 -0700

    Add some CSS to startpage and table

--- src/gameboard.js
+++ src/gameboard.js
@@ -95,23 +95,32 @@ class Gameboard {
 
     //   console.log("round", roundArray);
 
-
-
-//experiment
-
-    let xyz = this.firstRoundCategories.forEach(category => {
-      const cluesPerCat = this.cluesWithCategories.filter(clue => {
-        return clue.categoryId === data.categories[category]
-      })
-      
-    })
-  console.log(xyz)
-
     // this.firstRoundCategories = this.cluesWithCategories.find();
     // this.secondRoundCategories = this.categoryList.splice(0, 4);
     // this.finalRoundCategory = this.categoryList.splice(0, 1);
 
     // domUpdates.labelCategories([this.firstRoundCategories], [this.secondRoundCategories], [this.finalRoundCategory]);
+
+//experiment
+
+ let categoryArray0 = this.cluesWithCategories.filter(clue => {
+    return clue.categoryId === 1;
+ })
+ console.log(categoryArray0);
+
+    // console.log(this.firstRoundCategories);
+
+    // let xyz = this.firstRoundCategories.forEach(category => {
+    //   const cluesPerCat = this.cluesWithCategories.filter(clue => {
+    //     return clue.categoryId === data.categories[category]
+    //   })
+      
+    // })
+    // console.log(xyz);
+
+
+
+    
   
   
 
