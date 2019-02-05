a8c639bad344b40703b88e6bd12052bd7f4f8c85
raechel <odom.raechel@gmail.com>
2019-02-04 19:41:51 -0700

    addition to domUpdates

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,5 +1,31 @@
 import Gameboard from './GameBoard';
+import $ from 'jquery';
+
 
 export default {
+
+  toggleStart() {
+    let $p1Name = $("#p1-name-change-js")
+    $p1Name.text($('#p1-name-js').val())
+    let $p2Name = $("#p2-name-change-js")
+    $p2Name.text($('#p2-name-js').val())
+    let $p3Name = $("#p3-name-change-js")
+    $p3Name.text($('#p3-name-js').val())
+    $('.start-up').toggle();
+    $('.q-pop').hide();
+    $('.overlay').toggle();
+  },
+
+  toggleOverlay() {
+    $('.overlay').show();
+    $('.').show();
+  },
+
+  questionOverlay() {
+    $('.question-container').show();
+    $('.question-container').css('visibility', 'visible');
+    $('.question-overlay').toggleClass('hidden');
+  },
+
   
 }
