540522d5d9cbb55a6929c19fd74c3274e9ab18e2
Jake <jakeadmire1@gmail.com>
2019-02-06 13:13:44 -0700

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -19,47 +19,20 @@ import $ from 'jquery';
 
 
 // jQuery Variables
-const $playerOneOutput = $('#player1-output');
-const $playerTwoOutput = $('#player2-output');
-const $playerThreeOutput = $('#player3-output');
-
-const $player1Name = $('#player1');
-const $player2Name = $('#player2');
-const $player3Name = $('#player3');
-
-
-
-const $submitNames = $('.submit-names');
-const $spinWheel = $('.spin-wheel');
-const $buyVowel = $('.buy-vowel');
 const $solvePuzzle = $('.solve-puzzle');
-
-// JS variables
-// const difficulty1 = data.puzzles.one_word_answers;
-// const difficulty2 = data.puzzles.two_word_answers;
-// const difficulty3 = data.puzzles.three_word_answers;
-// const difficulty4 = data.puzzles.four_word_answers;
 
 let game;
 let player;
 
-// 
+$('.hidden-popup').hide();
+
 $('.consonant, .vowel').on('click', (event) => {
-  // console.log('BUTTON PRESSED');
   game.guessLetter(event);
 })
 
-
-$('.hidden-popup').hide();
-// jQuery Event Listeners
-$submitNames.on( 'click', playGame );
-$spinWheel.on('click', choosePrize );
-$buyVowel.on('click', buyVowel )
-
-// change to arrow function
-function playGame(e) {
+$('.submit-names').on( 'click', (e) => {
   e.preventDefault();
-  if ( $player1Name[0].value && $player2Name[0].value && $player3Name[0].value ) {
+  if ( $('#player1')[0].value && $('#player2')[0].value && $('#player3')[0].value ) {
     game = new Game();
     buildGame();
     $(".name-input").fadeOut(1000);
@@ -67,25 +40,29 @@ function playGame(e) {
     console.log('fill in names');
     // $promptWarning.toggle;
   }
-}
+});
 
+$('.spin-wheel').on('click', () => {
+  game.roundWheel.spinWheel();
+})
+
+$('.buy-vowel').on('click', (e) => {
+  e.preventDefault();
+  game.buyVowel();
+})
+// 
+// 
+// 
 function buildGame() {
   createPlayerNames();
   game.newRound();
-  // console.log(game);
-    // game.createWheel();
-    // game.createPuzzle();
-}
-
-function choosePrize() {
-  game.roundWheel.spinWheel();
 }
 
 function createPlayerNames() {
   const playerNames = [];
-  playerNames.push($player1Name.value);
-  playerNames.push($player2Name.value);
-  playerNames.push($player3Name.value);
+  playerNames.push($('#player1').value);
+  playerNames.push($('#player2').value);
+  playerNames.push($('#player3').value);
   game.createPlayers(playerNames);
   displayPlayerNames();
   game.scoreUpdate();
@@ -93,93 +70,7 @@ function createPlayerNames() {
 
   // MOVE TO DOM UPDATES
 function displayPlayerNames() {
-  $playerOneOutput.text($player1Name[0].value);
-  $playerTwoOutput.text($player2Name[0].value);
-  $playerThreeOutput.text($player3Name[0].value);
+  $('#player1-output').text($('#player1')[0].value);
+  $('#player2-output').text($('#player2')[0].value);
+  $('#player3-output').text($('#player3')[0].value);
 }
-
-function buyVowel(e) {
-  e.preventDefault();
-  game.buyVowel();
-}
-// newWheel() {
-//   const wheel = new Wheel();
-//   wheel.randomizeWheel();
-  //   display wheel on DOM;
-// }
-
-// newPuzzle() {
-//   const puzzle = new Puzzle(difficulty);
-//   puzzle.randomPuzzle();
-//   display puzzle, category, and hint on DOM;
-// }
-
-
-// Player ‘x’ turn
-// ---------------
-
-// $spinWheelButton.on( 'click', spinWheel() );
-
-// wheel.spinWheel() {
-//   spin wheel;
-//   let currentSpin = spin value;
-//   player.wheelResults(currentSpin);
-// }
-
-// player.wheelResults(spin) {
-//   if ( spin === 'bankrupt' ) {
-//     player.roundScore = 0; 
-//   } else if ( spin.type === number ) {
-//     player.roundScore += spin;
-//     player.chooseConsonant();
-//   }
-//   player.endTurn();
-// }
-
-
-// $solvePuzzleButton.on( 'click', solvePuzzle() );
-
-// puzzle.solvePuzzle() {
-//   prompt player to attempt to solve;
-//   $submitGuessButton.on( 'click', submitGuess() );
-// }
-
-// puzzle.submitGuess() {
-//   if ( guess !== this.answer ) {
-//     console.log('incorrect answer');
-//     player.endTurn();
-//   } else if ( guess === this.answer ) {
-//     this.totalScore += this.roundScore;
-//     game.roundOver();
-//   }
-// }
-
-
-// game.roundOver() {
-//   if ( round === 4 )
-//     this.bonusRound();
-//   else if ( round === bonus )
-//     this.endGame();
-//   else
-//     this.round++;
-//     this.startRound();
-// }
-
-
-// game.bonusRound() {
-//   new BonusWheel object;
-//   disable non-1st-place players
-// }
-
-
-// game.startRound() {
-// }
-
-
-// player.endTurn() {
-// }
-
-
-// game.endGame() {
-// }
-
