85ace158d3c3deece3f6963466de7d8b1feaf6e6
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-06 22:51:26 -0700

    fonts

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -69,7 +69,7 @@ const domUpdates = {
 
   displayTurnValue(value, activePlayer) {
     // let turnValue = wheel.turnValue
-    $('#spin-value').text(`${activePlayer.name} You landed on: ${value}!`)
+    $('#spin-value').text(`${activePlayer.name}, you landed on: ${value}!`)
   },
 
   updateRoundScore(value, num) {
