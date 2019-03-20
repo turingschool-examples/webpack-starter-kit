47a3aeed59e8802ba8287f86b24e562b4f3694c2
raechel <odom.raechel@gmail.com>
2019-03-20 16:13:38 -0600

    Player class set up and tests on default states
    Co-authored-by: Nim Garcia <nimm.garcia@gmail.com>

---
+++ src/js/player.js
@@ -0,0 +1,10 @@
+class Player {
+  constructor(name, answer) {
+    this.name = name;
+    this.answer = answer;
+    this.currentScore = 0;
+    this.totalScore = 0;
+  }
+}
+
+export default Player;
