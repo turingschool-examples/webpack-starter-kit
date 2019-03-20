57c28714f761360dba567f8a066e498c687cf070
RyanDBarnett <conner2468@gmail.com>
2019-03-20 16:06:49 -0600

    Add retrieve survey test

--- test/round-test.js
+++ test/round-test.js
@@ -2,12 +2,18 @@ import chai from 'chai';
 const expect = chai.expect;
 
 describe('Round class tests', function() {
-  it('should have a question property', function() {
+  it('should have a survey property', function() {
     const round = new Round();
-    expect(round).to.have.property(question);
+    expect(round).to.have.property(survey);
   });
   it('should have answers property', function() {
     const round = new Round();
     expect(round).to.have.property(answers);
+  });
+  it('should by default retrieve a survey object from gamedata and assign it to the round\'s survey property', function() {
+    const round = new Round();
+    expect(round.survey).to.be.an('object');
+    expect(round.survey).to.have.property('id');
+    expect(round.survey).to.have.property('question');
   });
 });
