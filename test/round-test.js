1b99fc7bb120d41406ec3dabdf3ea549412a5023
RyanDBarnett <conner2468@gmail.com>
2019-03-20 16:23:00 -0600

    Create seperate descriptions for methods and properties

--- test/round-test.js
+++ test/round-test.js
@@ -2,22 +2,26 @@ import chai from 'chai';
 const expect = chai.expect;
 
 describe('Round class tests', function() {
-  it('should have a survey property', function() {
-    const round = new Round();
-    expect(round).to.have.property(survey);
+  desribe('Property Tests', function() {
+    it('should have a survey property', function() {
+      const round = new Round();
+      expect(round).to.have.property(survey);
+    });
+    it('should have answers property', function() {
+      const round = new Round();
+      expect(round).to.have.property(answers);
+    });
+    it('should have a survey object assigned to the survey property', function() {
+      const round = new Round();
+      expect(round.survey).to.be.an('object');
+      expect(round.survey).to.have.property('id');
+      expect(round.survey).to.have.property('question');
+    });
   });
-  it('should have answers property', function() {
-    const round = new Round();
-    expect(round).to.have.property(answers);
-  });
-  it('should have a getSurvey method', function() {
-    const round = new Round();
-    expect(round).to.respondTo('getSurvey');
-  });
-  it('should by default retrieve a survey object from gamedata and assign it to the round\'s survey property', function() {
-    const round = new Round();
-    expect(round.survey).to.be.an('object');
-    expect(round.survey).to.have.property('id');
-    expect(round.survey).to.have.property('question');
+  describe('Method Tests', function() {
+    it('should have a getSurvey method', function() {
+      const round = new Round();
+      expect(round).to.respondTo('getSurvey');
+    });
   });
 });
