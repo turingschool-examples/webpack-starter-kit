eaf01b1b0c27d30b799ad41299ece567f69df654
Kayla Lawson <kaylarae7128@gmail.com>
2019-03-20 16:22:19 -0600

    Write test for player and round add dataset
    Co-authored-by: Jacqueline <jacquelineBmoore@gmail.com>

--- test/Round-test.js
+++ test/Round-test.js
@@ -2,3 +2,18 @@ import chai from 'chai'
 const expect = chai.expect;
 
 import Round from '../src/Round.js'
+
+describe ('Round', function() {
+    let round;
+    beforeEach(function() {
+        round = new Round(); 
+    });
+
+    it('should have clues', function () {
+        expect(round.clues).to.deep.equal(round.clues, []); 
+    })
+
+    it('should be currentPlayer in round', function() {
+        expect(round.currentPlayer).to.deep.equal(round.currentPlayer, {});
+    })
+})
