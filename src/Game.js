253c9c1d1c858c994584664dc1680aec26cdaa94
Colby Allen <colbyallen012@gmail.com>
2019-03-20 17:02:47 -0600

    Add spin method to wheel class.
    Co-authored-by: Duy Vu <duyv.thanh@gmail.com>

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import Player from './Player.js'
+import Wheel from './Wheel.js'
 
 class Game {
     constructor() {
@@ -9,10 +10,12 @@ class Game {
 
     }
     createPlayers(name1, name2, name3) {
+            let wheel = new Wheel()
             let player1 = new Player(name1);
             let player2 = new Player(name2);
             let player3 = new Player(name3);
             this.players.push(player1, player2, player3)
+            wheel.spinWheel()
     }
 
 }
--- src/Wheel.js
+++ src/Wheel.js
@@ -1,8 +1,17 @@
+import data from './data.js';
+
 class Wheel {
     constructor(){
         this.randomNumbers = [];
     }
     spinWheel() {
-        
+        let randomIndex = Math.floor(Math.random() * (data.wheel.length-1) + 0)
+        let newNum = data.wheel.find( (cur, idx) => {
+            return idx === randomIndex
+        })
+        console.log(newNum)
+        return newNum
     }
 }
+
+export default Wheel;
