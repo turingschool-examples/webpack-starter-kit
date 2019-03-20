e03ce8806d109b63ea9fcc9c155cbda96fa1b71b
Jarrett Kong <jarrettkong@me.com>
2019-03-20 15:57:08 -0600

    Added initial tests for Game, Round, and Player
    Co-authored by:
    Brennan Duffey <brennanduffey@gmail.com>

---
+++ test/Round-test.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+import spies from 'chai-spies';
+import Game from '../src/Game';
+import Round from '../src/Round';
+import Player from '../src/Player';
+import domUpdates from '../src/domUpdates'
+
+chai.use(spies);
+const assert = chai.assert;
+
+describe('Round', () => {
+
+  it('should have an id', () => {
+
+  });
+
+  // should have id, num, no default
+  // should have question, string, no default
+  // should have responses, array
+
+  // getAnswer?
+  // startRound
+});
