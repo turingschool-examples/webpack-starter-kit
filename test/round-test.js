180bee86ed72e2ba895f73f25a502b5464a04c0f
RyanDBarnett <conner2468@gmail.com>
2019-03-20 15:50:08 -0600

    Add question and answers property test

--- test/round-test.js
+++ test/round-test.js
@@ -2,5 +2,12 @@ import chai from 'chai';
 const expect = chai.expect;
 
 describe('Round class tests', function() {
-
+  it('should have a question property', function() {
+    const round = new Round();
+    expect(round).to.have.property(question);
+  });
+  it('should have answers property', function() {
+    const round = new Round();
+    expect(round).to.have.property(answers);
+  });
 });
