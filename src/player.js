f4384f9fbeb1ee76f4f5ce45635c2e930920a5ba
Max Silver <Maxbsilver@gmail.com>
2019-03-20 16:17:46 -0600

    Add data file data_wheel-of-fortune.js

---
+++ src/player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor(name){
+        this.name = name;
+        this.roundCaps = 0;
+        this.totalCaps = 0;
+        this.ans = '';
+    }
+}
+
+export default Player;
--- src/puzzle.js
+++ src/puzzle.js
@@ -1,6 +1,6 @@
 class Puzzle {
     constructor(){
-
+        
     }
 }
 
