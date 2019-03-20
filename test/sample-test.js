b46632e620173b71cd7bbc0d7489f59eb9025817
DeMarcus Kirby <tkd024@gmail.com>
2019-03-20 17:38:09 -0600

    added player class and tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,8 +1,20 @@
 import chai from 'chai';
+import Player from '../src/Player.js'
 const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
+describe('Player', function() {
+  it('should have a name', function() {
+    let player = new Player('Bob');
+    expect(player.name).to.equal('Bob');
+  });
+
+  it('should have a score of 0 as default', function() {
+    let player = new Player('Kevin');
+    expect(player.score).to.equal(0);
+  });
+
+  it('should have an empty array of correct answers by default', function(){
+    let player = new Player('Kirby')
+    expect(player.correctAnswers.length).to.equal(0);
   });
 });
