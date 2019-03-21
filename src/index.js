b28595ac633c2c340e1ba6582ee9f29b147ffb99
Sally Haefling <sallyhaefling@gmail.com>
2019-03-21 16:43:24 -0600

    Add click handler method in domUpdates and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file
+import domUpdates from './domUpdates.js';
 
 // An example of how you import jQuery into a JS file if you use jQuery in the file
 import $ from 'jquery';
+
 
 // An example of how you tell webpack to apply a CSS file
 import './css/base.css';
@@ -13,3 +15,9 @@ import Game from './Game';
 console.log('This is the JavaScript entry file - your code begins here.');
 
 let game = new Game();
+
+$('.start-button').on('click', function() {
+  domUpdates.displayName();
+  $('.start-button').hide('slow');
+  // $('.quit-game').prop('disabled', false);
+});
