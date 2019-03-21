680a87d6bb525cedf5ab8fd334cde31623ea9ed3
Katherine Lewis <kale8432@colorado.edu>
2019-03-20 18:57:11 -0600

    Initial JS, classes, and pseudocode
    co-authored-by: Hannah Hyde <hannahhyde@gmail.com>

---
+++ src/Player.js
@@ -0,0 +1,24 @@
+// class Player {
+//   constructor(name) {
+//     this.score = 0;
+//     this.name = name; //might be []
+//   }
+
+//   switchPlayer() {
+   //if player = player 1
+   //switch to player 2
+   //else player 2
+   //switch to player 1
+//   }
+    // score() {
+    //     game.checkPlayer()
+    //     const newScore = game.answers[i].response;
+    //     this.score += this.newScore['player1${currentPlayer}']
+    // }
+
+      //fastMoneyScore() {
+            //game.checkPlayer()
+    //     const newScore = game.answers[i].response;
+    //     this.score += (this.newScore['player1${currentPlayer}'] * input)
+     //}
+// }
---
+++ src/Round.js
@@ -0,0 +1,22 @@
+// class Round {
+//   constructor(id, question) {
+        // this.id = id;
+        // this.question = question;
+        // correctAnswer = 0;
+//   }
+        //needs to happen each new round
+        // getAnswer() {
+          // const responses = [];
+        //   if (id = id) {
+        //     grab answer
+        //     grab resp
+        // push into responses array
+        // sort by value of respondents id (highest first)
+        // map.random.splice of array [1-15] to get random question id to pick question (array is reset upon new game not new round) 
+      //}
+
+        //changeQuestionSet() {
+          //change inner text
+        //}
+        // }
+// }
