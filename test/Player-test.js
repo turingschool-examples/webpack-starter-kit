5dd004ce31ff4129e7d8671c417b1584f13e25cf
Emily Dittmer <emilyedittmer@gmail.com>
2019-03-20 22:54:40 -0600

    Lint run and error resolve with initial code

--- test/Player-test.js
+++ test/Player-test.js
@@ -3,32 +3,26 @@ import Player from '../src/Player.js';
 
 const expect = chai.expect;
 
-describe('See if the tests are running', function(){
-  it('should return true', function(){
+describe('See if the tests are running', function() {
+  it('should return true', function() {
     expect(true).to.equal(true);
   });
 
-  it('should have a name', function(){
+  it('should have a name', function() {
     let player = new Player('Lauren');
 
     expect(player.name).to.equal('Lauren');
   });
 
-  it('should have a default name', function(){
+  it('should have a default name', function() {
     let player = new Player();
 
     expect(player.name).to.equal('Player');
   });
 
-  it('should have a default score', function(){
+  it('should have a default score', function() {
     let player = new Player('Lauren');
 
     expect(player.score).to.equal(0);
-  });
-
-  it('should have a score', function(){
-    let player = new Player('Lauren', 20);
-
-    expect(player.score).to.equal(20);
   })
 })
