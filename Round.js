c27e4e58c8db092e95aaaedde11d659a6d98700e
Lynne Rang <lynne.rang@gmail.com>
2019-03-21 08:54:43 -0600

    Add Round and FastRound classes w/ initial properties and method ideas.

---
+++ Round.js
@@ -0,0 +1,49 @@
+class Round {
+    constructor(survey, roundNum = 0) {
+        this.survey = survey;
+        this.surveyAnswers = data.answers.filter(answer => answer.surveyId === surveyId); //maybe move the work to the game FN and pass result in here
+        this.guesses = [];
+        this.roundNum = roundNum;
+    }
+
+    checkGuess() {
+        //check against prev guesses
+            //if a previous guess, say it's already been guessed and try again.  clear input.
+            //if not guessed before, saveGuess() and checkifAnswer()
+    }
+        
+    saveGuess() {
+        //push guess into guesses array
+    }
+
+    checkIfAnswer() {
+        //compare against answer array
+            //if answer and answers.length is < 1
+                //display on board
+                //sort answers on board
+                //increment player score
+                //clear input
+                //pop answer out of answer array
+            //if answer and answers.length IS 1
+                //check roundNum, and if 3 => checkForWinner()
+                //check round, num and if < 3 => game.startNewRound()
+            //if not answer, clear the input and toggleActivePlayer()
+    }
+
+    toggleActivePlayer() {
+        //if active player is p1, active player = p2, else active player is p1.
+        //run fn to indicate active player in the dom
+    }
+    
+
+    displayAnswer() {
+        //maybe for Index.js, for updating answer in the dom.
+        //sortAnswers();
+    }
+
+    sortAnswers() {
+        //maybe for Index.js, for sorting answers displayed in the dom by respondent num.
+    }
+}
+
+export default Round;
--- src/index.js
+++ src/index.js
@@ -10,4 +10,8 @@ import './css/base.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import Round from 'Round.js';
+
+import Round from 'FastRound.js';
+
 console.log('This is the JavaScript entry file - your code begins here.');
