e8093b707518401cee13530c63fc26545a1401c3
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-03-20 20:45:54 -0600

    added to FinalRound, DailyDouble, and Game primarily
    Co-Authored-By: Kayla <kaylarae7128@gmail.com>

---
+++ src/FinalRound.js
@@ -0,0 +1,19 @@
+class FinalRound //extends Round 
+{
+  constructor() {
+    // super();
+    this.wager = 0;
+    this.maxWager = 0;
+  }
+  getClue() {
+    // selects final clue from clue array
+  }
+  chooseWinner() {
+    // sort players' scores
+    // player with highest score selected
+     // ${players.name} wins displayed
+    // if tie, getClue()? tiebreaker?
+  }
+}
+
+export default FinalRound;
--- test/FinalRound-test.js
+++ test/FinalRound-test.js
@@ -2,3 +2,19 @@ import chai from 'chai'
 const expect = chai.expect;
 
 import FinalRound from '../src/FinalRound.js'
+// import Round from '../src/Round.js';
+
+describe('FinalRound', function() {
+  let finalRound;
+  beforeEach(function() {
+    finalRound = new FinalRound();
+  });
+
+it('should have a wager starting at 0', function() {
+    expect(finalRound.wager).to.equal(0);
+  });
+
+it('should have a maximum wager starting at 0', function() {
+    expect(finalRound.maxWager).to.equal(0);
+  });
+})
