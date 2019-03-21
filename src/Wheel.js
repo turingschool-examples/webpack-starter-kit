5e8c42ee1263c33876f9278fb482cdee12f11b4d
Sally Haefling <sallyhaefling@gmail.com>
2019-03-21 16:04:09 -0600

    Add classes and testing and extra styling

---
+++ src/Wheel.js
@@ -0,0 +1,17 @@
+class Wheel {
+  constructor(values, currentIndex) {
+    this.values = [];
+    this.currentIndex = [];
+  }
+
+  getRandomWheel() {
+    //use randomizer to create random wheel
+  }
+
+  getWheelValue() {
+    //clean data to grab wheel values
+  }
+
+}
+
+export default Wheel;
