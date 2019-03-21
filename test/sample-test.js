4a8c489f9916ee04b5ca55195f42787052dbf02d
Max Silver <Maxbsilver@gmail.com>
2019-03-20 18:57:27 -0600

    Update test files

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,8 +1,8 @@
-import chai from 'chai';
-const expect = chai.expect;
+// import chai from 'chai';
+// const expect = chai.expect;
 
-describe('See if the tests are running', function() {
-  it('should return true', function() {
-    expect(true).to.equal(true);
-  });
-});
+// describe('See if the tests are running', function() {
+//   it('should return true', function() {
+//     expect(true).to.equal(true);
+//   });
+// });
