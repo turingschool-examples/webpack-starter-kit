b9694a2f5bfee3813a854cbdb8b8906fbefbf48a
Max Silver <Maxbsilver@gmail.com>
2019-03-20 19:27:16 -0600

    Add properties and testing to class BonusRound

--- test/dom-updates-test.js
+++ test/dom-updates-test.js
@@ -4,3 +4,13 @@ import DomUpdates from '../src/dom-updates.js';
 
 import spies from 'chai-spies';
 chai.use(spies);
+
+describe('DomUpdates', ()=>{
+    it('Should be a function', ()=>{
+        expect(DomUpdates).to.be.a('function');
+    })
+    it('Should instanciate a new instance of DomUpdates', ()=>{
+        const domUpdates =  new DomUpdates();
+        expect(domUpdates).to.be.an('object');
+    })
+})
