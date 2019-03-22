17e34323a85e14cfebfbf506743ef2de632617de
Katherine Lewis <kale8432@colorado.edu>
2019-03-21 22:02:19 -0600

    Beginning of JS
    Co-authored-by: Hannah Hyde <hannahhyde@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,12 +6,39 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to apply a CSS file
 import './css/base.css';
-import './css/normalize.css'
+import './css/normalize.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import './images/turing-logo.png';
+
+//import dataset
+import '/dataset.js';
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+function getDatasetAnswers() {
+   const dataAnswers = data.answers;
+   for (let i=0; i<dataAnswers.length; i++) {
+    surveys.push(dataAnswers);
+      // console.log(dataAnswers[i]);
+      // console.log(dataAnswers[i].surveyId);
+        // console.log(dataAnswers[i].respondents);
+          // console.log(dataAnswers[i].answer);
+   } 
+};
+
+function getDatasetQuestions() {
+    const dataQuestions = data.surveys;
+   for (let i=0; i<dataQuestions.length; i++) {
+    surveys.push(dataQuestions)
+      // console.log(dataQuestions[i]);
+      //console.log(dataQuestions[i].id);
+        // console.log(dataQuestions[i].question);  
+   } 
+};
+
+var surveys = [];
+
 
 //function submitButton {
 //   if(wrong answer) {
@@ -25,11 +52,11 @@ console.log('This is the JavaScript entry file - your code begins here.');
 // function createGame() {
 //   var game = new Game
 //   instantiate new game (new array, new players, new round)
-manipulate the original datasets to pair questions with their correct responses
- -- responses are sorted in order of highest respondents to lowest
-on instatiation of a new game:
-iterate over original combined datasets
-push into new array game.surveys
-every call for a new survey question (every round) splices from the array in Game
-when instantiation deletes (game is over), the copy array goes with it
+// manipulate the original datasets to pair questions with their correct responses
+//  -- responses are sorted in order of highest respondents to lowest
+// on instatiation of a new game:
+// iterate over original combined datasets
+// push into new array game.surveys
+// every call for a new survey question (every round) splices from the array in Game
+// when instantiation deletes (game is over), the copy array goes with it
 // }
