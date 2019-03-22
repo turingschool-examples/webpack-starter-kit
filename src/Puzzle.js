ce20df275c15b428c6d8266d32429bac0614aae5
Bridgett Coyle <bacoyle0409@gmail.com>
2019-03-21 19:53:47 -0600

    Add more functionality to player

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -1,26 +1,15 @@
 import data from './data.js';
 
 class Puzzle {
-  constructor(letterBank, vowels, categories, description, splitAnswer) {
+  constructor(letterBank, vowels, description, splitAnswer) {
     this.letterBank = null;
     this.vowels = [];
-    this.categories = []; 
     this.description = null;
     this.splitAnswer = null;
   }
 
   getRandomPuzzle() {
     
-  }
-  getRandomCategories() {
-    for(let i = this.categories.length - 1; i>0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [this.categories[i], this.categories[j]] = 
-      [this.categories[j], this.categories[i]];
-    }
-  //     data.puzzles.one_word_answers.puzzle_bank.map(cat => {
-  // return cat.category;
-  // });
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,19 +5,24 @@ import domUpdates from './domUpdates.js';
 // An example of how you import jQuery into a JS file if you use jQuery in the file
 import $ from 'jquery';
 
+import data from './data.js';
+
 
 // An example of how you tell webpack to apply a CSS file
 import './css/base.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import Game from './Game';
+import Game from './Game.js';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
 let game = new Game();
+console.log(game);
+console.log(data);
+
 
 $('.start-button').on('click', function() {
-  domUpdates.displayName();
+  domUpdates.startGame(game);
   $('.start-button').hide('slow');
   // $('.quit-game').prop('disabled', false);
 });
