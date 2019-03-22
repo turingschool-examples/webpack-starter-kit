3ec399451ea8aaf81701520a0b9793fe804915a5
Kevin Krato <kratok08@gmail.com>
2019-03-22 08:06:06 -0600

    Add pseudocode

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,25 @@
 class Game {
   constructor(){
-    this.currentTurn = 'p1';
+    this.currentTurn = '';
   }
   startGame(){
-
+    //import the data structures
+    //copy the data structures
+    //select 1 survey at random and remove it from the source array
+    //append the question to the DOM
+    //create a array with the three associated answers and remove them from source array
   }
   restartGame(){
-
+    //clear all fields
+    //revert back to starting arrays
   }
   whoseTurn(){
     if (this.currentTurn === 'p2'){
-      this.currentTurn = 'p1';
+      //inputs and fields should reflect that it's players X's turn
+      //player Y disabled
     } else {
-      this.currentTurn = 'p2';
+      //inputs and fields should reflect that it's players Y's turn
+      //player X disabled
     }
   }
 }
