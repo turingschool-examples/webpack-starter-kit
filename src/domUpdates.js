127a5bb115ffee730a04be60ade6e6227714ba3b
Hannah Hyde <hannahhyde@gmail.com>
2019-03-23 16:37:41 -0600

    Add instatiation of new game and new players
    co-authored-by: Katie Lewis <kale8432@colorado.edu>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,30 +2,26 @@ import $ from 'jquery';
 import Game from './Game.js';
 
 
+export default {
 
-$(window).on('load', popUp);
+  // popUp() {
+  // let startPopUp = $('.gamePopUp');
+  // startPopUp.innerHTML += `<section class="startGamePopUp">
+  //    <h2>Welcome to Family Feud!</h2>
+  //    <p>Please enter player names.</p>
+  //     <label for="">Player 1 Name</label>
+  //    <input class="nameOne"></input>
+  //    <label for="">Player 2 Name</label>
+  //    <input class="nameTwo"></input>
+  //    <button id="startBtn">Start Game</button>
+  //   </section>`
+// console.log('heyyyy')
+// },
+//   test() {
+//     console.log(" test method ")
+  
+}
 
-console.log('what up');
-
-function popUp() {
-  let startPopUp = $('.gamePopUp');
-  startPopUp.innerHTML += `<section class="startGamePopUp">
-     <h2>Welcome to Family Feud!</h2>
-     <p>Please enter player names.</p>
-      <label for="">Player 1 Name</label>
-     <input class="nameOne"></input>
-     <label for="">Player 2 Name</label>
-     <input class="nameTwo"></input>
-     <button id="startBtn">Start Game</button>
-    </section>`
-
-console.log('heyyyy')
-};
-
-
-
-
-// export default domUpdates;
 
 //update scores
 //update questions
