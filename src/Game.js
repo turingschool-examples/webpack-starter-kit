5c268b17e170065c37713174da0a2cc56dcdfdcd
Kevin Krato <kratok08@gmail.com>
2019-03-23 12:33:56 -0600

    Create linked answers to question array

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,5 @@
 import gameData from './data.js';
+import domObject from './DOMupdates.js';
 
 class Game {
   constructor(){
@@ -7,14 +8,24 @@ class Game {
   }
   startGame(){
     //select 1 survey at random and remove it from the source array
-    const gameDataCopy = gameData.surveys.slice();
+    const copiedAnswers = gameData.answers.slice();
+    const copiedQuestions = gameData.surveys.slice();
     const randomNumber = genNumber();
-    const questionObject = gameDataCopy.splice(randomNumber, 1);
-    function genNumber() { return Math.floor(Math.random() * gameDataCopy.length) + 1 };
+    const questionObject = copiedQuestions.splice(randomNumber, 1);
+    const questionString = questionObject[0].question;
+    const answersArray = [];
+    function genNumber() { return Math.floor(Math.random() * copiedQuestions.length) + 1 };
     
-    const question = questionObject[0].question;
-    return question;
+    //potentially need to move this function to Round object.
     //append the question to the DOM
+    domObject.createQuestion(questionString);
+
+    copiedAnswers.forEach( element => {
+      if (element.surveyId === questionObject[0].id) { answersArray.push(element) };
+    })
+    // console.log(questionObject[0].id);
+    // console.log(copiedAnswers);
+    // console.log(answersArray)
     //create a array with the three associated answers and remove them from copied array
   }
   restartGame(){
