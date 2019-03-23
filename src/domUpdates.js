1fd3d65cb73bc411d0c0510fe3d824265983177d
Katherine Lewis <kale8432@colorado.edu>
2019-03-23 17:11:43 -0600

    Added popup and removed popup
    co-authored-by: Hannah Hyde <hannahhyde@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -4,21 +4,22 @@ import Game from './Game.js';
 
 export default {
 
-  // popUp() {
-  // let startPopUp = $('.gamePopUp');
-  // startPopUp.innerHTML += `<section class="startGamePopUp">
-  //    <h2>Welcome to Family Feud!</h2>
-  //    <p>Please enter player names.</p>
-  //     <label for="">Player 1 Name</label>
-  //    <input class="nameOne"></input>
-  //    <label for="">Player 2 Name</label>
-  //    <input class="nameTwo"></input>
-  //    <button id="startBtn">Start Game</button>
-  //   </section>`
-// console.log('heyyyy')
-// },
-//   test() {
-//     console.log(" test method ")
+  popUp() {
+  $('.gamePopUp').append( `<section class="startGamePopUp">
+     <h2>Welcome to Family Feud!</h2>
+     <div class='playerNames'>
+     <p>Please enter player names.</p>
+      <label for="">Player 1 Name</label>
+     <input class="nameOne"></input>
+     <label for="">Player 2 Name</label>
+     <input class="nameTwo"></input>
+     </div>
+     <button id="startBtn">Start Game</button>
+    </section>`
+)
+  console.log('heyyyy')
+},
+
   
 }
 
