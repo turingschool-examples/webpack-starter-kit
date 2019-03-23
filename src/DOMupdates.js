00b8861dd58500027002f2115ba2435fa84dd5b4
Kevin Krato <kratok08@gmail.com>
2019-03-23 09:36:20 -0600

    Migrate JQuery and create getUserInput method

--- src/DOMupdates.js
+++ src/DOMupdates.js
@@ -1,5 +1,20 @@
 import $ from 'jquery';
 
 export default {
- //refer to spies lesson;
+
+	getUserInput() { 
+			$(".user-inputs").on( "click", () => {
+			$(".main-content").slideDown();
+			$(".user-inputs").hide();
+		})
+
+		const $playerOneName = $('#player-1-name');
+		const $playerTwoName = $('#player-2-name');
+
+		//after 3 guesses are complete show restart game button
+		$(".player-cards").on("click", () => {
+			$(".restart-game").slideDown(300);
+		})
+	}
+
 }
