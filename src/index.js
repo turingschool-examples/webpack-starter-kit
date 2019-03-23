8df68f952dad08f3af20352fc30bc0de8b03a028
Bridgett Coyle <bacoyle0409@gmail.com>
2019-03-23 17:47:50 -0600

    Add random wheel values to wheel js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import $ from 'jquery';
 import data from './data.js';
 import './css/base.css';
 import Game from './Game.js';
+import Wheel from './Wheel.js';
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
 let game = new Game();
+let wheel = new Wheel();
 console.log(game);
 
+
 $('.start-button').on('click', function() {
-  domUpdates.startGame(game);
+  domUpdates.startGame(game, wheel);
   domUpdates.displayCategoryName(game);
   $('.start-button').hide('slow');
   $('.button').prop('disabled', false).css('color', 'white');
@@ -19,8 +22,15 @@ $('.start-button').on('click', function() {
 });
 
 $('.quit-button').on('click', function() {
- domUpdates.resetGame();
- domUpdates.disableQuit();
- $('.button').prop('disabled', true).css('color', 'grey');
- $('.player-name-input').show();
+ // domUpdates.resetGame();
+  domUpdates.disableQuit();
 });
+
+$('.quit-button').on('click', function() {
+  location.reload(true);
+  });
+
+$('#js-spin-button').on('click', function() {
+  wheel.getRandomWheel();
+});
+
