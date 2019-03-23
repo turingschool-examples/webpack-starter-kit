cf308145b1ea233da6e99e256e24fbca91f80646
Kevin Krato <kratok08@gmail.com>
2019-03-23 17:23:03 -0600

    Migrate event listeners to index.js

--- src/index.js
+++ src/index.js
@@ -13,12 +13,41 @@ import './images/turing-logo.png';
 
 //import objects
 import Game from './Game.js';
+import Round from './Round.js';
 let a = new Game();
+let b = new Round();
+
+//is this right??? i dont think so
 a.startGame();
 
 
 import events from './DOMupdates.js'
-events.generalEvents();
+// events.listenForGuess();
+
+
+//listeners
+$("#submit-names").on( "click", () => {
+	if($('#player-1-name').val() == '' || $('#player-2-name').val() == ''){
+		return;
+	} else {
+	$(".main-content").slideDown();
+	$(".user-inputs").hide();
+	}
+});
+
+//capture value from guess input
+$("#submit-guess").on("click", event => {
+	event.preventDefault();
+	let userInput = $("#player-guess").val()
+	console.log("userInput: " + userInput);
+});
+
+//after 3 guesses are complete show restart game button
+$(".player-cards").on("click", () => {
+	$(".restart-game").slideDown(300);
+});
+
+
 // $playerOneName.addClass('hidden');
 
 console.log('This is the JavaScript entry file - your code begins here.');
