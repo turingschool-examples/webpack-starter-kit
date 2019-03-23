fba0b6dd41c491113b3ab9438372d5c90e33da33
Katherine Lewis <kale8432@colorado.edu>
2019-03-22 18:13:40 -0600

    Friday To-dos

--- test/sample-test.js
+++ test/sample-test.js
@@ -6,3 +6,17 @@ describe('See if the tests are running', function() {
     expect(true).to.equal(true);
   });
 });
+
+
+
+//Hannah will complete fastMoney test
+//Katie will complete Game test
+//Both will commit and do a PR
+//Both will log one round of current issues on github
+//Hannah refactor html (issue)
+//Katie add css (issue)
+//Both will attempt one piece of functionality
+//Both will practice codewars/jsfun
+//Both check trello board
+
+//Due by 10AM
