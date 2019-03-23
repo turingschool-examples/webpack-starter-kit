9fae53b08e48dac6a66fe87261f3ea807997acd4
Kevin Krato <kratok08@gmail.com>
2019-03-23 13:09:18 -0600

    Append answers and associated points to dom

--- src/DOMupdates.js
+++ src/DOMupdates.js
@@ -21,6 +21,18 @@ export default {
 		const $currentQuestion = $("#current-question");
 		$currentQuestion.html(element);
 		console.log('am i real?')
+	},
+
+	createAnswers(a1,a1points,a2,a2points,a3,a3points) {
+		$(".answer-1").html(a1);
+		$(".answer-1-pts").html(a1points);
+
+		$(".answer-2").html(a2);
+		$(".answer-2-pts").html(a2points);
+
+		$(".answer-3").html(a3);
+		$(".answer-3-pts").html(a3points);
+		console.log('dom test answers');
 	}
 
 }
