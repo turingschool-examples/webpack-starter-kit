10c0f41d444c856cba168d42adbe4875f4fa6aa1
Kelly Zick <kllyzck@gmail.com>
2019-03-23 13:27:31 -0600

    Commented out very short one word puzzle and line in game.js to test game board with longer word lengths

--- src/Game.js
+++ src/Game.js
@@ -35,8 +35,8 @@ class Game {
       // bonuswheel
     }
     let round = new Round(this.players);
-    let currentPuzzle = round.getPuzzle(this.allData[this.roundCount]);
-    // let currentPuzzle = round.getPuzzle(this.allData[this.roundCount - 1]);
+    // let currentPuzzle = round.getPuzzle(this.allData[this.roundCount]);
+    let currentPuzzle = round.getPuzzle(this.allData[this.roundCount - 1]);
     domUpdates.appendPuzzle(currentPuzzle, currentPuzzle.splitAnswer);
   }
 
