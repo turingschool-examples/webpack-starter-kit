6a252b9ffa24f1b4046c5bb85b7e6492e10f9684
Kevin Krato <kratok08@gmail.com>
2019-03-23 09:40:39 -0600

    Import structs and test structs access

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,14 +1,29 @@
 import chai from 'chai';
-import Game from '../src/Game.js'
+import Game from '../src/Game.js';
+import gameData from '../src/data.js';
 const expect = chai.expect;
 
 describe('Game', function() {
-  it('should swap players turn', function() {
-    let game = new Game();
-    expect(game.currentTurn).to.equal('p1');
-    game.whoseTurn();
-    expect(game.currentTurn).to.equal('p2');
-    game.whoseTurn();
-    expect(game.currentTurn).to.equal('p1');
-  });
-});
+	it('Should have access to the data structures', function() {
+		let myGame = new Game();
+		expect(myGame.game).to.equal(gameData);
+	})
+	// it('Should copy the data structures', function() {
+	// 	let game = new Game();
+	// 	expect(game).to.equal(true);
+	// })
+	// it('Should remove a survey question at random from original array', function() {
+
+	// })
+})
+
+// describe('Game', function() {
+//   it('should swap players turn', function() {
+//     let game = new Game();
+//     expect(game.currentTurn).to.equal('p1');
+//     game.whoseTurn();
+//     expect(game.currentTurn).to.equal('p2');
+//     game.whoseTurn();
+//     expect(game.currentTurn).to.equal('p1');
+//   });
+// });
