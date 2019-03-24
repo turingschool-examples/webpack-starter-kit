fba57dec3638b7a26b532c592a8ec03a71214c92
Emily Dittmer <emilyedittmer@gmail.com>
2019-03-24 10:19:41 -0600

    DO NOT MERGE

---
+++ src/Round.js
@@ -0,0 +1,23 @@
+import Game from './Game.js';
+
+class Round {
+  constructor() {
+    this.categories = [];
+  }
+
+  displayCategories() {
+    const showNames = ['United States History',
+     'Life Sciences', 'Public Health', 'Education Jargon', 
+     'Name That Board Game', 'American Literature',
+      'Biographies', 'American Cities', 'Food', 'Cable TV'];
+  }
+
+  randomizeCategories(){
+    //letroundOneCategories []
+    //math.random # x 4
+    //place #s into array
+    //if # is in array, generate another number
+  }
+}
+
+export default Round;
