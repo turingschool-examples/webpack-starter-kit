da26ec721ff807982910dc8e87781d8148e69bde
tkbacher <tiffanybacher@gmail.com>
2019-03-24 12:38:04 -0600

    Click names and guess on enter

--- src/index.js
+++ src/index.js
@@ -17,6 +17,23 @@ import './images/family-feud-logo.png'
 import Game from './Game.js';
 import Player from './Player.js';
 
+// Dom Updates
+import domUpdates from './domUpdates.js';
+
+$("#player-1-input:text:visible:first").focus();
+
+$("#player-1-input").keypress(function(e) {
+  if (e.keyCode === 13) {
+    $("#player-2-input").focus();
+  }
+});
+
+$("#player-2-input").keypress(function (e) {
+  if (e.keyCode === 13) {
+    $("#submit-names-btn").click(); 
+  }
+}); 
+
 $("#submit-names-btn").on("click", function() { 
   const player1Name = $("#player-1-input").val();
   const player2Name = $("#player-2-input").val();
@@ -24,6 +41,7 @@ $("#submit-names-btn").on("click", function() {
   $(".player-1-name").text(player1Name.toUpperCase());
   $(".player-2-name").text(player2Name.toUpperCase());
   $(".welcome-screen").addClass("hidden");
+  $(".guess-input").focus();
 
   let startingPlayer = Math.floor(Math.random() * 2) + 1
 
@@ -32,11 +50,21 @@ $("#submit-names-btn").on("click", function() {
   window.game.toggleActivePlayer();
 });
 
+$(".guess-input").keypress(function (e) {
+  if (e.keyCode === 13 && $(".guess-input").val() !== '') {
+    $("#submit-guess-btn").click(); 
+  }
+}); 
+
 $("#submit-guess-btn").on("click", function() { 
-  const playerGuess = $(".guess-input").val().toLowerCase();
-  window.game.round.saveGuess(playerGuess);
-  window.game.round.checkAnswer(playerGuess, window.game);
-  $(".guess-input").val('');
+  if ($(".guess-input").val() !== '') {
+    const playerGuess = $(".guess-input").val().toLowerCase();
+    window.game.round.saveGuess(playerGuess);
+    window.game.round.checkAnswer(playerGuess, window.game);
+    $(".guess-input").val('');
+  } else {
+    domUpdates.showMustEnterGuessMsg();
+  }
 });
 
 $("#start-btn").on("click", function() {
