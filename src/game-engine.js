9b5f5e048007818fcf63275f5dc1b9f366042c00
Max Silver <Maxbsilver@gmail.com>
2019-03-25 10:42:55 -0600

    Each round, round caps are converted to total caps for the player who won the round

--- src/game-engine.js
+++ src/game-engine.js
@@ -16,6 +16,14 @@ class GameEngine {
     // ToDo:  Update roundCaps to Zero on DOM 
     // move newRound to round
     this.currentRound.roundNumber++;
+    // console.log('total caps : ', game.currentRound.currentPlayer.totalCaps)
+    // console.log('round caps : ', game.currentRound.currentPlayer.roundCaps)
+    game.currentRound.currentPlayer.totalCaps = game.currentRound.currentPlayer.roundCaps;
+    game.players.map(player => console.log(player))
+    game.players.map(player=> player.roundCaps = 0);
+    game.players.map(player => console.log(player))
+
+
     game.currentRound.allRoundGuesses = [];
     this.currentRound.determinePuzzleLength();
     this.currentRound.displayDomPuzzle(game);
