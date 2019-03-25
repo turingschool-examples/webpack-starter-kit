34785f37c45d3fa2f76aa9dff6f817e7a7597b6c
DeMarcus Kirby <tkd024@gmail.com>
2019-03-25 16:53:28 -0600

    added addition testing suites

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,30 +1,34 @@
 import chai from 'chai';
 import Game from '../src/Game.js';
+import Round from '../src/Round.js';
 import gameData from '../src/data.js';
+import $ from 'jQuery';
 const expect = chai.expect;
 
 describe('Game', function() {
-	it('Should have access to the data structures', function() {
-		let myGame = new Game();
-		expect(myGame.game).to.equal(gameData);
+	it('Should initiate with player1s turn', function(){
+	let game = new Game();
+	expect(game.currentTurn).to.equal('player1');
 	})
-	it('Should randomly splice a survey question and return the question as a string', function() {
-		let myGame = new Game();
-		expect(myGame.startGame()).to.be.a('String');
-	})
-	it('Source gameData object array\'s should not be mutated', function() {
-		expect(gameData.surveys.length).to.equal(15);
-		expect(gameData.answers.length).to.equal(45);
-	})
-})
 
-// describe('Game', function() {
-//   it('should swap players turn', function() {
-//     let game = new Game();
-//     expect(game.currentTurn).to.equal('p1');
-//     game.whoseTurn();
-//     expect(game.currentTurn).to.equal('p2');
-//     game.whoseTurn();
-//     expect(game.currentTurn).to.equal('p1');
-//   });
-// });
+	it('Should swap turns', function() {
+	let game = new Game();
+	game.whoseTurn();
+	expect(game.currentTurn).to.equal('player1');
+	game.whoseTurn();
+	expect(game.currentTurn).to.equal('player2');
+	game.whoseTurn();
+	expect(game.currentTurn).to.equal('player1');
+	})
+
+	it('Should have an answer array with length', function() {
+	let game = new Game();
+	game.startGame();
+	expect(game.currentAnswer.length).to.equal(3);
+	})
+
+	it('Source gameData object array\'s should not be mutated', function() {
+	expect(gameData.surveys.length).to.equal(15);
+	expect(gameData.answers.length).to.equal(45);
+	})
+});
