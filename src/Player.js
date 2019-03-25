de955b14ad11691b50b3e7342c7bc029a4530626
jacobogart <jacobogart@gmail.com>
2019-03-25 13:39:31 -0600

    Add Player class helper functions
    Co-authored-by: Ryan Barnett <conner2468@gmail.com>

--- src/Player.js
+++ src/Player.js
@@ -11,16 +11,24 @@ class Player {
   makeGuess(guess, game, round) {
     if (typeof guess !== 'string') {
       return 'Error: Argument Not String';
-    } else if (round.answers.map(a=>a.answer.toUpperCase()).includes(guess.toUpperCase())) {
-      const correctAnswer = round.answers.find(a => a.answer.toUpperCase() === guess.toUpperCase());
-      this.score += correctAnswer.respondents;
-      domUpdates.revealCorrectAnswer(correctAnswer);
-      domUpdates.updateScore(this.playerNum, this.score);
-      domUpdates.showGuessMessage('correct');
+    } else if (this.checkGuess(round, guess)) {
+      this.correctGuess(round, guess);
     } else {
       domUpdates.showGuessMessage('wrong');
     }
     game.toggleIsTurn();
+  }
+
+  checkGuess(round, guess) {
+    return round.answers.map(a => a.answer.toUpperCase()).includes(guess.toUpperCase());
+  }
+
+  correctGuess(round, guess) {
+    const correctAnswer = round.answers.find(a => a.answer.toUpperCase() === guess.toUpperCase());
+    this.score += correctAnswer.respondents;
+    domUpdates.revealCorrectAnswer(correctAnswer);
+    domUpdates.updateScore(this.playerNum, this.score);
+    domUpdates.showGuessMessage('correct');
   }
 
 }
