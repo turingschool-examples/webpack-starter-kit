1abffdb9537cd476287132f33386bba10994d173
Colby Allen <colbyallen012@gmail.com>
2019-03-25 14:45:22 -0600

    Increment stages and add turns
    Co-authored-by: Duy Vu <duyv.thanh@gmail.com>

---
+++ test/player-test.js
@@ -0,0 +1,18 @@
+import chai from 'chai';
+import Player from '../src/Player.js';
+const expect = chai.expect;
+
+describe('Player', function() {
+    let player;
+    beforeEach(function () {
+        player = new Player();
+    });
+    it('should be an instance of player', function (){
+        expect(player).to.be.an.instanceof(Player);
+    });
+    it('should have default properties', function (){
+        expect(player.name).to.equal(player.name);
+        expect(player.score).to.equal(0);
+    })
+});
+
