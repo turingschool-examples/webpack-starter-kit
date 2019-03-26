e61654f2912280e501a8646888b52589d755b6b5
Kevin Krato <kratok08@gmail.com>
2019-03-26 10:20:05 -0600

    Lint files and add config plug for prettier

--- src/DOMupdates.js
+++ src/DOMupdates.js
@@ -1,21 +1,19 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 export default {
+  createQuestion(element) {
+    const $currentQuestion = $("#current-question");
+    $currentQuestion.html(element);
+  },
 
-	createQuestion(element) {
-		const $currentQuestion = $("#current-question");
-		$currentQuestion.html(element);
-	},
+  createAnswers(a1, a1points, a2, a2points, a3, a3points) {
+    $(".answer-1").html(a1);
+    $(".answer-1-pts").html(a1points);
 
-	createAnswers(a1,a1points,a2,a2points,a3,a3points) {
-		$(".answer-1").html(a1);
-		$(".answer-1-pts").html(a1points);
+    $(".answer-2").html(a2);
+    $(".answer-2-pts").html(a2points);
 
-		$(".answer-2").html(a2);
-		$(".answer-2-pts").html(a2points);
-
-		$(".answer-3").html(a3);
-		$(".answer-3-pts").html(a3points);
-	}
-
-}
+    $(".answer-3").html(a3);
+    $(".answer-3-pts").html(a3points);
+  }
+};
--- src/index.js
+++ src/index.js
@@ -18,55 +18,55 @@ import events from './DOMupdates.js'
 
 //listeners
 $("#submit-names").on( "click", () => {
-	if($('#player1-name-input').val() === '' || $('#player2-name-input').val() === ''){
-		return;
-	} else {
-		player1.name = $('#player1-name-input').val();
-		player2.name = $('#player2-name-input').val();
-		$('.player-1-name').html(player1.name);
-		$('.player-2-name').html(player2.name);
-		$('.current-turn').html(`${player1.name}'s turn!`);
-		$(".main-content").slideDown();
-		$(".user-inputs").hide();
-	}
+  if ($('#player1-name-input').val() === '' || $('#player2-name-input').val() === '') {
+    return;
+  } else {
+    player1.name = $('#player1-name-input').val();
+    player2.name = $('#player2-name-input').val();
+    $('.player-1-name').html(player1.name);
+    $('.player-2-name').html(player2.name);
+    $('.current-turn').html(`${player1.name}'s turn!`);
+    $(".main-content").slideDown();
+    $(".user-inputs").hide();
+  }
 });
 
 
 //capture value from guess input
 $("#submit-guess").on("click", event => {
-	event.preventDefault();
+  event.preventDefault();
 
-	if (game.currentRound !== 3) {
-		game.whoseTurn()
+  if (game.currentRound !== 3) {
+    game.whoseTurn()
 
-		//let player 1 guess first
-		if (game.currentTurn === 'player1') {
+    //let player 1 guess first
+    if (game.currentTurn === 'player1') {
 
-			$('.current-turn').html(`${player2.name}'s turn!`)
-			let userInput = $("#player-guess").val()
-			player1.score += (game.checkAnswers(userInput, game.currentAnswer));
-			$(".player-1-score").html(`Score: ${player1.score}`);
-		} else if (game.currentTurn === 'player2') {
+      $('.current-turn').html(`${player2.name}'s turn!`)
+      let userInput = $("#player-guess").val()
+      player1.score += (game.checkAnswers(userInput, game.currentAnswer));
+      $(".player-1-score").html(`Score: ${player1.score}`);
+    } else if (game.currentTurn === 'player2') {
 
-			$('.current-turn').html(`${player1.name}'s turn!`)
-			let userInput = $("#player-guess").val()
-			player2.score += (game.checkAnswers(userInput, game.currentAnswer));
-			$(".player-2-score").html(`Score: ${player2.score}`);
+      $('.current-turn').html(`${player1.name}'s turn!`)
+      let userInput = $("#player-guess").val()
+      player2.score += (game.checkAnswers(userInput, game.currentAnswer));
+      $(".player-2-score").html(`Score: ${player2.score}`);
 
-		}
-	} else {
-		console.log('Guess button prepped for round 3');
+    }
+  } else {
+    console.log('Guess button prepped for round 3');
 
 
-		//PLAYER 1 NEEDS A TIMER, IF ALL ANSWERs MET // TIMER = 0 end round
-		$('.current-turn').html(`${player1.name}'s turn!`)
-		let userInput = $("#player-guess").val()
-		player1.score += (game.checkAnswers(userInput, game.currentAnswer));
-		$(".player-1-score").html(`Score: ${player1.score}`);
+    //PLAYER 1 NEEDS A TIMER, IF ALL ANSWERs MET // TIMER = 0 end round
+    $('.current-turn').html(`${player1.name}'s turn!`)
+    let userInput = $("#player-guess").val()
+    player1.score += (game.checkAnswers(userInput, game.currentAnswer));
+    $(".player-1-score").html(`Score: ${player1.score}`);
 
-		// run round.generateRoundTimed();
+    // run round.generateRoundTimed();
 
-	}
+  }
 });
 
 $('.multiplier-form').on('click', '.multiplier-radio', (event) => {
@@ -82,7 +82,7 @@ $('.multiplier-form').on('click', '.multiplier-radio', (event) => {
 
 //after 3 guesses are complete show restart game button
 $(".player-cards").on("click", () => {
-	$(".restart-game").slideDown(300);
+  $(".restart-game").slideDown(300);
 });
 
 
