dba2dfd69d2d6661e756d90d742af89f0927856d
Max Silver <Maxbsilver@gmail.com>
2019-03-26 08:08:44 -0600

    index update

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -16,21 +16,29 @@ export default {
     // $(`.player-${currPlayer.playerNum}__current-points`).text(`${currPlayer.roundCaps}`);
     // $(`.player-${currPlayer.playerNum}__totals-points`).text(`${currPlayer.totalCaps}`);
     game.players.map((player, index) => {
-    $(`.player-${index + 1}__current-points`).text(`${player.roundCaps}`);
-    $(`.player-${index + 1}__total-points`).text(`${player.totalCaps}`);
+      $(`.player-${index + 1}__current-points`).text(`${player.roundCaps}`);
+      $(`.player-${index + 1}__total-points`).text(`${player.totalCaps}`);
     })
   },
-  showCurrentPlayer(game){
+  showCurrentPlayer(game) {
     let currPlayer = game.currentRound.currentPlayer;
     $(`.player-${currPlayer.playerNum}__name`).toggleClass('big-red-border');
   },
-  updateLettersUsed(game){
+  updateLettersUsed(game) {
+    // TODO: Refactor
     let lettersRemaining = $('.letters__remaining');
-    lettersRemaining.text(game.currentRound.allRoundGuesses);
+    lettersRemaining.html('');
+    let usedLetters = [];
+    usedLetters = game.currentRound.allRoundGuesses.filter(letter => {
+      return !usedLetters.includes(letter);
+    })
+    usedLetters.map(letter => {
+      lettersRemaining.append(`<p class="used-letter used-${letter}"> ${letter},</p>`)
+    });
+    // lettersRemaining.text(game.currentRound.allRoundGuesses);
   },
   updateRoundNumber(game) {
     $('#rnd-num').text(game.currentRound.roundNumber)
-    console.log("round-num: ", game.currentRound.roundNumber)
   },
   updateRoundHintCategory(game) {
     // console.log(game.currentRound.roundPuzzle);
@@ -40,7 +48,7 @@ export default {
     $('.hint__value').text(`Hint: ${game.currentRound.roundPuzzle.description}`);
     this.updateRoundNumber(game);
   },
-  appendPuzzle(game){
+  appendPuzzle(game) {
     // ! remove children of board--container
     // !!!
     let valueBoard = $('.board__tile-value--container');
@@ -48,7 +56,7 @@ export default {
     $('.letters__remaining').html('_');
     const alphabetArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     // console.log(game.currentRound.answer) puzzle array
-    game.currentRound.answer.map((letter, index)=>{
+    game.currentRound.answer.map((letter)=>{
       // console.log(game.currentRound.answer)
       // todo: remove index if it's not used
       if(alphabetArr.includes(letter.toUpperCase())){
@@ -56,10 +64,11 @@ export default {
       }
       else {
         valueBoard.append(`<p class="ans-letter fade-in">${letter}</p>`);
+
       }
     })
   },
-  createPuzzleClassArr(letter){
+  createPuzzleClassArr(letter) {
     $(`.letter-${letter.toUpperCase()}`).text(letter.toUpperCase());
     $(`.letter-${letter.toUpperCase()}`).addClass('fade-in-letter');
     //!! append a winner screen for the winner
