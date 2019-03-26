35a1a9e13e7258094f85ec87dfee5b65d936b214
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-03-26 10:56:58 -0600

    sorry kayla
    Co-authored-by: Kayla <kaylarae7128@gmail.com>

--- src/game.js
+++ src/game.js
@@ -42,7 +42,7 @@ class Game {
     if (this.currentPlayer === 3) {
       this.currentPlayer = 0;
     }
-    // } else if (this.currentPlayer = player)
+    // } else if (this.currentPlayer = player)f
     console.log('current player', this.players[this.currentPlayer]);
   }
 }
