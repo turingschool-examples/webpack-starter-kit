22d5ae647bf076888909b339b87eacc51562604f
RyanDBarnett <conner2468@gmail.com>
2019-03-26 15:06:28 -0600

    Create basic timer

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ let round;
 $(".name-btn").on("click", startGame);
 $(".guess-btn").on("click", guess);
 $(".guess-input").on("keydown", domUpdates.hideGuessMessages);
+$(".start-timer-btn").on("click", runTimer);
 
 function startGame() {
   const p1Name = $("#p1-name-input").val();
@@ -50,7 +51,10 @@ function guess() {
   game.player1.isTurn ? game.player1.makeGuess(guess, game, round) : game.player2.makeGuess(guess, game, round);
   domUpdates.clearInput();
   if (round.answers.length === 0) {
-    
     round = game.startNewRound();
   }
+}
+
+function runTimer() {
+  round.startTimer($(".timer"));
 }
