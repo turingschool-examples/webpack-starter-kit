ab60da1e88658fd362f974a3f3bd87fd735b2cf0
Michael King-Stockton <34406483+KStockton@users.noreply.github.com>
2019-03-27 16:20:57 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // Do not delete or rename this file
 
 // An example of how you import jQuery into a JS file if you use jQuery in the file
-import $ from 'jquery';3
+import $ from 'jquery';
 import Game from './Game'
 // import Data from './Data'
 // import Round from './Round'
