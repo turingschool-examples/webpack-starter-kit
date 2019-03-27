f9ddf1ad8a477c634a2a6e975a0dcfc358b54ee4
RyanDBarnett <conner2468@gmail.com>
2019-03-27 14:17:07 -0600

    Create result box css file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import $ from 'jquery';
 // An example of how you tell webpack to apply a CSS file
 import './css/normalize.css';
 import './css/base.css';
+import './css/modules/result-box.css';
 
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
