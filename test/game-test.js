a5b9f5b5d7fccbbb7baaeba0b272dc0c4c4bceb6
Colby Allen <colbyallen012@gmail.com>
2019-03-26 18:31:51 -0600

    wheel test

--- test/game-test.js
+++ test/game-test.js
@@ -1,6 +1,10 @@
 import chai from 'chai';
 import Game from '../src/Game.js';
+import domUpdates from '../src/domUpdates.js';
+import spies from 'chai-spies';
+chai.use(spies);
 const expect = chai.expect;
+
 
 describe('Game', function(){
     let game;
@@ -11,7 +15,9 @@ describe('Game', function(){
         expect(game).to.be.an.instanceOf(Game);
     });
     it('should have default properties', function (){
-        expect(game.turn).to.equal(0);
+        expect(game.players).to.deep.equal([]);
+        expect(game.usedLetters).to.deep.equal([]);
         expect(game.stage).to.equal(0);
+        expect(game.currentPlayer).to.equal(null);
     });
 });
--- test/wheel-test.js
+++ test/wheel-test.js
@@ -1,10 +1,30 @@
 import chai from 'chai';
 import Wheel from '../src/Wheel.js';
+import Game from '../src/Game.js';
+import domUpdates from '../src/domUpdates.js';
+import spies from 'chai-spies';
+chai.use(spies);
+
+chai.spy.on(domUpdates, 'showCurrentPlayer', () => true);
+chai.spy.on(domUpdates, 'startGame', () => true);
+chai.spy.on(domUpdates, 'changeNames', () => true);
+chai.spy.on(domUpdates, 'changeCategory', () => true);
+chai.spy.on(domUpdates, 'changeClue', () => true);
+chai.spy.on(domUpdates, 'checkLetterGuess', () => true);
+chai.spy.on(domUpdates, 'checkAnswerBoard', () => true);
+chai.spy.on(domUpdates, 'toggleSpin', () => true);
+chai.spy.on(domUpdates, 'resetNames', () => true);
+chai.spy.on(domUpdates, 'resetLetters', () => true);
+chai.spy.on(domUpdates, 'resetInputs', () => true);
+chai.spy.on(domUpdates, 'appendGuessCard', () => true);
+
 const expect = chai.expect;
 
 describe('Wheel', function (){
+    let game;
     let wheel;
     beforeEach(function (){
+        game = new Game();
         wheel = new Wheel();
     });
     it('should be an instance of wheel', function (){
@@ -12,6 +32,10 @@ describe('Wheel', function (){
     });
     it('should have default properties', function (){
         expect(wheel.currentSpinValue).to.equal(null);
-        expect(wheel.disabled).to.equal(false);
+        expect(wheel.spunValues).to.deep.equal([]);
+    });
+    it('should only have three players to start a game', function (){
+        game.startGame('Colby', 'Bailey', 'Allen');
+        expect(game.players.length).to.deep.equal(3);
     });
 });
