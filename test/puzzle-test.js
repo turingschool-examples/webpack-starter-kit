27d83af32b3f29e4c7b1c16856edd7e529f5fc83
Colby Allen <colbyallen012@gmail.com>
2019-03-27 10:27:04 -0600

    test game and puzzle

--- test/puzzle-test.js
+++ test/puzzle-test.js
@@ -1,5 +1,23 @@
 import chai from 'chai';
 import Puzzle from '../src/Puzzle.js';
+import data from '../src/data.js';
+import domUpdates from '../src/domUpdates.js';
+import spies from 'chai-spies';
+chai.use(spies);
+
+// chai.spy.on(domUpdates, 'showCurrentPlayer', () => true);
+// chai.spy.on(domUpdates, 'startGame', () => true);
+// chai.spy.on(domUpdates, 'changeNames', () => true);
+// chai.spy.on(domUpdates, 'changeCategory', () => true);
+// chai.spy.on(domUpdates, 'changeClue', () => true);
+// chai.spy.on(domUpdates, 'checkLetterGuess', () => true);
+// chai.spy.on(domUpdates, 'checkAnswerBoard', () => true);
+// chai.spy.on(domUpdates, 'toggleSpin', () => true);
+// chai.spy.on(domUpdates, 'resetNames', () => true);
+// chai.spy.on(domUpdates, 'resetLetters', () => true);
+// chai.spy.on(domUpdates, 'resetInputs', () => true);
+// chai.spy.on(domUpdates, 'appendGuessCard', () => true);
+
 const expect = chai.expect;
 
 describe('Puzzle', function(){
