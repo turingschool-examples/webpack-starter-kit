eb52e5c6c5edb93d576cce930439a0f36b8587c5
Max Silver <Maxbsilver@gmail.com>
2019-03-28 09:17:01 -0600

    Final lint fix

--- test/bonus-round-test.js
+++ test/bonus-round-test.js
@@ -1,8 +1,6 @@
 import chai from 'chai';
 const expect = chai.expect;
 import BonusRound from '../src/bonus-round.js';
-import Puzzle from '../src/puzzle.js'
-
 import spies from 'chai-spies';
 chai.use(spies);
 
