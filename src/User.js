6a858ed01e5d7090653b64cad8594396fdae2696
Anneke McGrady <annekemcgrady@gmail.com>
2019-05-18 16:30:55 -0600

    Create User class and User-test files

---
+++ src/User.js
@@ -0,0 +1,17 @@
+class User {
+  constructor(name, player = playerOne) {
+    this.name = name;
+    this.score = 0;
+    this.player = player;
+  }
+
+  updateScore() {
+    this.score += answers.respondents;
+    //DOM update display score
+  }
+
+  //DOM update name 
+
+}
+
+export default User;
