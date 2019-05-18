8c2621ecca0a3f7bdce336f1f00c24abb7803bbc
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-18 12:41:49 -0600

    instantiate Round Class and test properties/methods
    co-authored-by: Alek Aker <alekaker@outlook.com>
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>

--- src/Round.js
+++ src/Round.js
@@ -1,6 +1,17 @@
+import data from "../data";
+
 class Round {
   constructor () {
-    
+    this.surveys = (this.pullSurveys());
+  }
+  pullSurveys() {
+    const randomSurveys = []
+    const randomSurveyID = Math.floor(Math.random() * (14 - 2 + 1)) + 2;
+    let survey1 = data.surveys.find(survey => survey.id === randomSurveyID)
+    let survey2 = data.surveys.find(survey => survey.id === randomSurveyID - 1)
+    let survey3 = data.surveys.find(survey => survey.id === randomSurveyID + 1)
+    randomSurveys.push(survey1, survey2, survey3);
+    return randomSurveys
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 import Round from '../src/Round'
+import data from '../data'
 const expect = chai.expect;
 
 describe('Round', function () {
@@ -17,5 +18,14 @@ describe('Round', function () {
   it('should instantiate Round', function () {
     expect(round).to.be.an.instanceOf(Round)
   })
+
+  it('should hold surveys', function () {
+    expect(round.surveys).to.be.an('array')
+  })
+
+  it('should pull 3 random surveys', function () {
+    round.pullSurveys()
+    expect(round.surveys).to.have.length(3)
+  }) 
 
 });
