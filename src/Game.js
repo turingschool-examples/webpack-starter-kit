75268baaa403f0362c747cf1fb2a1c4d0bc9cef1
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-18 14:00:09 -0600

    instantiated Game class, tests and a method
    co-authored-by: Alek Aker <alekaker@outlook.com>
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,17 @@
-class Game {
-  constructor() {
+import Round from '../src/Round'
+import Player from './Player';
 
+class Game {
+  constructor(name1, name2) {
+    this.players = (this.newPlayers(name1, name2))
+    this.round = (new Round())
+  }
+  newPlayers (name1, name2) {
+    let bothPlayers = []
+    let player1 = new Player(1, name1)
+    let player2 = new Player(2, name2)
+    bothPlayers.push(player1, player2)
+    return bothPlayers
   }
 }
 
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,13 +1,15 @@
 import chai from 'chai';
 import Game from '../src/Game'
+import Round from '../src/Round'
 const expect = chai.expect;
 
 describe('Game', function () {
 
   let game;
-
+  let round;
   beforeEach(function () {
-    game = new Game()
+    game = new Game('Aidan', 'Patrick')
+    round = new Round()
   })
 
   it('should be a function', function () {
@@ -17,5 +19,18 @@ describe('Game', function () {
   it('should instantiate Game', function () {
     expect(game).to.be.an.instanceOf(Game)
   })
+
+  it('should instantiate a new Round', function () {
+    expect(game.round).to.be.an.instanceOf(Round)
+  })
+
+  it('should instantiate 2 players', function () {
+    expect(game.players).to.be.an('array')
+    expect(game.players).to.be.eql([
+      { id: 1, name: 'Aidan', score: 0, guess: undefined }, 
+      { id: 2, name: 'Patrick', score: 0, guess: undefined }])
+  })
+
+  
 
 });
