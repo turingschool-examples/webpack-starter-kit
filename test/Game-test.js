9d3aa47c33d99ab9595407090dcbad24cf4356d3
Vinton Teo <taulago@ymail.com>
2019-05-18 18:19:50 -0600

    Create test for Game class.
    Co-authored-by: Steve Rumizen <steverumizen@gmail.com>

--- test/Game-test.js
+++ test/Game-test.js
@@ -21,7 +21,33 @@ describe('Game', function() {
 
   it('should have default properties', function() {
     game = new Game(players);
-    expect(game.roundCounter).to.equal(0);
     expect(game.players.length).to.eql(3);
+    expect(game.roundCounter).to.equal(0);
+  });
+
+  it('should be able to return a puzzle block', function() {
+    expect(game.returnPuzzleBlock().length).to.equal(5);
+  });
+
+  it('should be able to instantiate a puzzle from the puzzle block', function() {
+    expect(game.instantiatePuzzle().correctAnswer).to.eql([...'Armchair']);
+  });
+
+  it('should be able to start a game', function() {
+    game.start();
+    expect(game.currentRound.puzzle.correctAnswer).to.eql([...'Armchair']);
+  });
+
+  it('should be able to declare a winner', function() {
+    player1.totalScore = 2000;
+    expect(game.roundCounter).to.equal(4);
+    game.findWinner();
+    expect(game.winner.name).to.equal('Steve');
+  });
+
+  it.skip('should be able to end the game', function() {
+    game.startBonusRound();
+    expect(game.roundCounter).to.equal(4);
+    //check for defaults of bonus round?
   });
 });
