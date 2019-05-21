793c42f9375115e417bc6fe7dd0d944f7b0b8e72
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-21 16:57:20 -0600

    add dom manipulation and button events
    co-authored-by: Alek Aker <alekaker@outlook.com>
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,29 @@
+/* eslint-disable object-shorthand */
 import $ from 'jquery'
 const domUpdates = {
-  displayWords (string) {
-    $('#words').text(string);
+
+  showBoard: function () {
+    console.log('new game')
+    $('#form_game-start').hide()
+    $('#game_board').toggle()
+    $('#input_player-2').prop('disabled', true)
+  },
+
+  assignNames: function (name1, name2) {
+    $("#name_player-1, label[for='input_player-1']").text(name1)
+    $("#name_player-2").text(name2)
+    $("label[for='input_player-guess']").text(name1 + ' guess')
+  },
+
+  checkGuess: function () {
+    // let player.guess = $('#input_player-guess').val()
+
+  },
+
+  switchPlayer: function (player) {
+    $("label[for='input_player-guess']").text(`${player.name} guess`)
   }
+
 }
 
 export default domUpdates
--- src/index.js
+++ src/index.js
@@ -14,5 +14,43 @@ import Round from './Round';
 import Turn from './Turn';
 import Player from './Player';
 import Survey from './Survey';
+import domUpdates from './domUpdates';
+
+$(document).ready(function () {
+  $('#game_board').hide()
+})
+
+let game;
+
+$('#btn_game-start').on('click', function (e) {
+  e.preventDefault()
+  console.log('prevent')
+  let player1 = $('#input_name-player1').val()
+  let player2 = $('#input_name-player2').val()
+  if (player1 && player2) {
+    game = new Game(player1, player2)
+    domUpdates.showBoard()
+    domUpdates.assignNames(player1, player2)
+    console.log(game.players)
+    console.log(game.round.surveys)
+    console.log(game.round.turn.answers)
+  } else {
+    alert('enter a name dipshit')
+  }
+})
+
+$('#btn_submit').on('click', function (e) {
+  e.preventDefault()
+  console.log('button')
+  if (game.round.turn.currentPlayer === 1) {
+    game.players[0].guess = $('#input_player-guess').val()
+    game.round.turn.checkGuess(game.players[0])
+    console.log('player 1')
+  } else {
+    game.players[1].guess = $('#input_player-guess').val()
+    game.round.turn.checkGuess(game.players[1])
+    console.log('player 2')
+  }
+})
 
 
