96b24286e017bc0132772e9eaacab7b963ddebe7
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-20 18:23:34 -0600

    refactored Round and test to be able to test for random
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>
    co-authored-by: Alek Aker <alekaker@outlook.com>

--- src/Round.js
+++ src/Round.js
@@ -2,18 +2,26 @@ import data from "../data";
 import Turn from "./Turn";
 import domUpdates from './domUpdates';
 class Round {
-  constructor () {
-    this.surveys = (this.pullSurveys());
+  constructor (number) {
+    this.randomNum = number || Math.floor(Math.random() * (14 - 2 + 1)) + 2;
+    this.surveys = (this.pullSurveys(this.randomNum));
     this.turn = (new Turn(this))
+    this.currentRound = 1;
   }
-  pullSurveys() {
+  pullSurveys(randomSurveyID) {
     const randomSurveys = []
-    const randomSurveyID = Math.floor(Math.random() * (14 - 2 + 1)) + 2;
+    // const randomSurveyID = Math.floor(Math.random() * (14 - 2 + 1)) + 2;
     let survey1 = data.surveys.find(survey => survey.id === randomSurveyID)
     let survey2 = data.surveys.find(survey => survey.id === randomSurveyID - 1)
     let survey3 = data.surveys.find(survey => survey.id === randomSurveyID + 1)
     randomSurveys.push(survey1, survey2, survey3);
     return randomSurveys
+  }
+
+  changeRound() {
+    turn.answers.length === 0
+      ? this.currentRound++
+      : null;
   }
 }
 
