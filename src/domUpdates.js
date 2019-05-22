08cfb24ea247bce6056b3302a9ebef0ac9354fa1
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-21 20:53:20 -0600

    add method to hide names and dipslay respondents
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -2,35 +2,37 @@
 import $ from 'jquery'
 const domUpdates = {
 
-  showBoard: function (roundObj) {
-    console.log('new game')
+  showBoard: function (roundObj, index) {
     $('#form_game-start').hide()
-    $('#game_board').toggle()
-    $('#survey_output').text(roundObj.surveys[0].question)
+    $('#game_board').show()
+    $('#survey_output').text(roundObj.surveys[index].question)
+    this.hideAnswers()
   },
 
   assignNames: function (name1, name2) {
     $("#name_player-1, label[for='input_player-1']").text(name1)
     $("#name_player-1").text(name1)
     $("#name_player-2").text(name2)
-    // $("label[for='input_player-guess']").text(name1 + ' guess')
   },
 
   checkGuess: function () {
-    // let player.guess = $('#input_player-guess').val()
     $('#input_player-guess').val('That\'s already been guessed')
   },
 
   correctAnswer: function (player, score, answerObj, num) {
     $(`#score_player-${player}`).text(score)
-    $(`.survey-${num}`).text(answerObj.answer)
+    $(`.survey-${num}`).html(`${answerObj.answer} ---- ${answerObj.respondents}`)
   },
 
   switchPlayer: function () {
     $('#steve1').toggle()
     $('#steve2').toggle()
-    // $("label[for='input_player-guess']").text(`${player.name} guess`)
-    // $(`#name_player-${player.id}`).css('border', '1px solid red')
+  },
+
+  hideAnswers: function () {
+    $('.survey-0').text('Hidden answer 1')
+    $('.survey-1').text('Hidden answer 2')
+    $('.survey-2').text('Hidden answer 3')
   }
 
 }
