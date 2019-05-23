863c14fae55e22d74981d089a86cf8bf1c754aaf
Emily Dittmer <emilyedittmer@gmail.com>
2019-05-22 19:11:23 -0600

    Add normalize.scss file
    Co-Authored-by: Andreea Hanson <andreea.hanson7@gmail.com>
    Co-Authored-by: Anneke McGrady <annekemcgrady@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@
 import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
+import './css/normalize.scss'
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
