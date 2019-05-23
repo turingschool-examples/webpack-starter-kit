4a009aa523d2893864deb19ffe658b33c25f7b59
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-22 18:30:13 -0600

    refactored to instantiate new turn on all survey questions guessed
    co-authored-by: Aidan McKay <aidanmckay2000@gmail.com>

--- src/Round.js
+++ src/Round.js
@@ -5,25 +5,23 @@ class Round {
   constructor (number) {
     this.randomNum = number || Math.floor(Math.random() * (14 - 2 + 1)) + 2;
     this.surveys = (this.pullSurveys(this.randomNum));
-    this.turn = (new Turn(this))
     this.currentRound = 1;
+    this.turn = new Turn(this.surveys[this.currentRound - 1])
   }
+
   pullSurveys(randomSurveyID) {
     const randomSurveys = []
-    // const randomSurveyID = Math.floor(Math.random() * (14 - 2 + 1)) + 2;
-    let survey1 = data.surveys.find(survey => survey.id === randomSurveyID)
-    let survey2 = data.surveys.find(survey => survey.id === randomSurveyID - 1)
-    let survey3 = data.surveys.find(survey => survey.id === randomSurveyID + 1)
+    let survey1 = data.surveys.find(survey => survey.id === randomSurveyID);
+    let survey2 = data.surveys.find(survey => survey.id === randomSurveyID - 1);
+    let survey3 = data.surveys.find(survey => survey.id === randomSurveyID + 1);
     randomSurveys.push(survey1, survey2, survey3);
-    return randomSurveys
+    return randomSurveys;
   }
 
   changeRound() {
     if (this.turn.guessed.length === 3) {
-      console.log('new round son')
       this.currentRound++
-      this.surveys.shift()
-      new Turn(this)
+      this.turn = new Turn(this.surveys[this.currentRound - 1])
       domUpdates.showBoard(this, (this.currentRound - 1))
     }
   }
--- src/Turn.js
+++ src/Turn.js
@@ -5,16 +5,17 @@ import Game from '../src/Game';
 import domUpdates from './domUpdates';
 
 class Turn {
-  constructor(round) {
-    this.answers = (this.findAnswers(round));
+  constructor(survey) {
+    this.answers = (this.findAnswers(survey));
     this.currentPlayer = 1;
     this.guessed = []
   }
 
-  findAnswers (round) {
+  findAnswers (survey) {
     let answers = [... data.answers]
-    let filteredAnswers = answers.filter(steve => steve.surveyId === round.surveys[0].id)
+    let filteredAnswers = answers.filter(steve => steve.surveyId === survey.id)
     let sortedAnswers = filteredAnswers.sort((a, b)=> b.respondents - a.respondents)
+    console.log(sortedAnswers)
     return sortedAnswers
   }
 
