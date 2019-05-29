ce4c8c604dd81bb36d5f831bb37afa5daecaa7e4
Steve Rumizen <steverumizen@gmail.com>
2019-05-29 16:54:46 -0600

    Add passing roomService tests

--- src/RoomService.js
+++ src/RoomService.js
@@ -2,6 +2,25 @@ class RoomService {
   constructor(data) {
     this.data = data;
   }
+
+  returnAllRoomServices() {
+    return this.data;
+  }
+
+  returnDailyTotalSpent(givenDate) {
+    const forThisDate = this.data.filter(day => day.date === givenDate);
+    return Math.round(100 * forThisDate.reduce((sum, order) => {
+      sum += order.totalCost;
+      return sum;
+    }, 0)) / 100;
+  }
+
+  returnAllTimeTotalSpent() {
+    return Math.round(100 * this.data.reduce((sum, order) => {
+      sum += order.totalCost;
+      return sum;
+    }, 0)) / 100;
+  }
 }
 
 export default RoomService;
--- test/RoomService-test.js
+++ test/RoomService-test.js
@@ -1,10 +1,28 @@
 import chai from 'chai';
 const expect = chai.expect;
-import RoomService from '../src/RoomService.js';
 import RoomServiceRepo from '../src/RoomServiceRepo.js';
+import roomServiceData from '../data/roomServiceData.js';
 
 describe('RoomService', function () {
-  it('should return true', function () {
-    expect(true).to.equal(true);
+  let roomServiceRepo;
+
+  beforeEach(function () {
+    roomServiceRepo = new RoomServiceRepo(roomServiceData);
+  });
+
+  it('should have default properties', function () {
+    expect(roomServiceRepo.returnUserRoomService(97).data.length).to.equal(3);  
+  });
+
+  it('Should be able to return all room services', function () {
+    expect(roomServiceRepo.returnUserRoomService(97).returnAllRoomServices().length).to.equal(3);
+  });
+
+  it('Should be able to return the total amount spent on a given day', function() {
+    expect(roomServiceRepo.returnUserRoomService(97).returnDailyTotalSpent('22/10/2019')).to.equal(43.4);
+  });
+
+  it('Should return the total spent all time by user', function() {
+    expect(roomServiceRepo.returnUserRoomService(97).returnAllTimeTotalSpent()).to.equal(43.4);
   });
 });
