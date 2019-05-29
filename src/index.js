6799f7b1a45cf1a6b646182d47e542c87137b018
“Hindreen” <hndfaw@gmail.com>
2019-05-29 17:56:11 -0600

    Add feching to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,40 @@ import $ from 'jquery';
 import fetch from 'cross-fetch';
 
 
+var usersData, roomsData, bookingsData, roomServicesData;
 
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/users/users')
+  .then(response => response.json())
+  .then(data => {
+    usersData = data.users;
+  });
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/rooms/rooms')
+  .then(response => response.json())
+  .then(data => {
+    roomsData = data.rooms;
+  });
+  
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/bookings/bookings')
+  .then(response => response.json())
+  .then(data => {
+    bookingsData = data.bookings;
+  });
+
+  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1903/room-services/roomServices')
+  .then(response => response.json())
+  .then(data => {
+    roomServicesData = data.roomServices;
+  });
+
+  
+
+function timer() {
+  console.log(usersData);
+  console.log(roomsData);
+  console.log(bookingsData);
+  console.log(roomServicesData);
+}
+setTimeout(timer, 200);
 
 
