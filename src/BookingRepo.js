6567b7955f386f8a385e0fad337070ce95366d6e
Steve Rumizen <steverumizen@gmail.com>
2019-05-29 16:23:24 -0600

    Add bookingRepo passing tests

--- src/BookingRepo.js
+++ src/BookingRepo.js
@@ -1,6 +1,14 @@
 class BookingRepo {
-  constructor() {
+  constructor(data) {
+    this.data = data;
+  }
 
+  returnModeBookingDate() {
+    const dateTally = this.data.bookings.reduce((final, booking) => {
+      !final[booking.date] ? final[booking.date] = 1 : final[booking.date]++;
+      return final;
+    }, {});
+    return Object.keys(dateTally).find(date => dateTally[date] === Math.max(...Object.values(dateTally)));
   }
 }
 
--- test/BookingRepo-test.js
+++ test/BookingRepo-test.js
@@ -4,7 +4,17 @@ import BookingRepo from '../src/BookingRepo.js';
 import bookingData from '../data/bookingData.js';
 
 describe('BookingRepo', function () {
-  it('should return true', function () {
-    expect(true).to.equal(true);
+  let bookingRepo;
+
+  beforeEach(function () {
+    bookingRepo = new BookingRepo(bookingData);
+  });
+
+  it('should have default properties', function () {
+    expect(bookingRepo.data.bookings.length).to.equal(200);
+  });
+
+  it('Should be able to find the most popular booking date', function() {
+    expect(bookingRepo.returnModeBookingDate()).to.equal('29/09/2019')
   });
 });
