b1d665c070d230184395e945ad1f408623a62d81
“Hindreen” <hndfaw@gmail.com>
2019-05-29 17:54:58 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
+// import './css/base.scss';
+// import './images/turing-logo.png'
+import fetch from 'cross-fetch';
 
-// An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+
