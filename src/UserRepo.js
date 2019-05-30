856dc3b5e50575bd6bf6c09aae7d3354dada2941
Steve Rumizen <steverumizen@gmail.com>
2019-05-30 11:30:50 -0600

    Add method to create new customer with correct id

--- src/UserRepo.js
+++ src/UserRepo.js
@@ -6,6 +6,12 @@ class UserRepo {
   returnUser(name) {
     return this.data.users.find(user => user.name === name);
   }
+
+  createNewUser(name) {
+    const ids = this.data.users.map(user => user.id);
+    const highestId = Math.max(...ids);
+    return {id: highestId + 1, name: name}
+  }
 }
 
 export default UserRepo;
--- test/UserRepo-test.js
+++ test/UserRepo-test.js
@@ -14,7 +14,11 @@ describe('UserRepo', function() {
       expect(userRepo.data.users.length).to.equal(100);
     });
 
-    it('Should be able to return a user object given ', function() {
+    it('Should be able to return a user object given a name', function() {
       expect(userRepo.returnUser('Kianna Walter').id).to.equal(17);
     });
+
+  it('Should be able to return a new user object given a name', function () {
+    expect(userRepo.createNewUser('Steve Rumizen').id).to.equal(101);
+  });
 });
