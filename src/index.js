1ba510e6f2d2dc2fa33e4ba44f64b977a50f77f8
RyanFlachman <ryan@flachman.net>
2019-05-31 09:02:11 -0600

    Add tab icons

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import $ from 'jquery';
 import './css/base.scss';
 import Customer from './customer';
 import domUpdates from './domupdates'
+import './images/breakfast.svg'
+import './images/specialist-user.svg'
+import './images/open-book.svg'
 
 $(document).ready(function() {
-  domUpdates.hideAtStart()
-})
+	
+  $('.nav-list__tab').click(function() {
+    var tab_id = $(this).attr('data-tab');
 
-$('#main__customer-section').on('click', function() {
-  domUpdates.displayCustomerTab()
+    $('.nav-list__tab').removeClass('current');
+    $('.tab-content').removeClass('current');
+
+    $(this).addClass('current');
+    $("#"+tab_id).addClass('current');
+  })
+
 })
 
 
