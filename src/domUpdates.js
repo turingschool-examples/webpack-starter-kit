d34e78a53a4d9e0699881d4643a5b7d9457ae2e5
Roger Hughes <Rogerallenhughes@hotmail.com>
2019-09-12 11:44:21 -0600

    Add functionality to tab switching

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,1 +1,18 @@
 // This fucks with the DOM. ONLY THIS FILE FUCKS WITH THE DOM
+import $ from 'jquery';
+// Tab shit
+export default {
+  startOnMainTab() {
+$('.section__display article').hide();
+$('.section__display article:first').show();
+$('.main__section--tabs li:first').addClass('tab-active');
+},
+
+  changeTabs(event, _this) {
+  event.preventDefault();
+  $('.section__display li').removeClass('tab-active');
+  $(_this).parent().addClass('tab-active');
+  $('.section__display article').hide();
+  $($(_this).attr('href')).show();
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import domUpdates from './domUpdates'
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+domUpdates.startOnMainTab();
+
+$('.main__section--tabs a').on('click', function(event) {
+let _this = this;
+domUpdates.changeTabs(event, _this);
+})
