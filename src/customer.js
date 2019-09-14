ea57ecb5e9258d792383f17347b431ff602b0933
Roger Hughes <Rogerallenhughes@hotmail.com>
2019-09-14 14:15:26 -0600

    Add styling for theme

--- src/customer.js
+++ src/customer.js
@@ -11,57 +11,6 @@ class Customer {
     this.currentDish;
     this.currentPrice;
   }
-  
-
-  calculateAllBookings() {
-    if(this.bookings.length > 0) {
-      let roomNums = this.bookings.map(booking => booking.roomNumber);
-      let roomCosts = roomNums.map(number => {
-        return this.rooms.find(room => room.number === number).costPerNight;
-      });
-      return roomCosts.reduce((total, cost) => {
-        return total += cost;
-      }, 0)
-    } else {
-      return 0;
-    }
-  }
-
-  calculateAllRoomService() {
-    if(this.roomService.length > 0) {
-      let costs = this.roomService.reduce((totalCost, order) => {
-          totalCost += order.totalCost;
-        return totalCost;
-      }, 0)
-      return parseFloat(costs.toFixed(2));
-    } else {
-      return 0;
-    }
-  }
-
-  calculateAllCosts() {
-    let costs = this.calculateAllBookings() + this.calculateAllRoomService();
-    return parseFloat(costs.toFixed(2));
-  }
-
-  orderRoomService(food, date) {
-    let dish = this.menu.find(plate => plate.food === food);
-    this.roomService.push({
-      userID: this.id,
-      date: date,
-      food: dish.food,
-      totalCost: dish.totalCost
-    })
-  }
-
-  cancelRoomService(food, date) {
-   let plateIndex = this.roomService.findIndex(order => order.food === food && order.date === date);
-   this.roomService.splice(plateIndex, 1);
-  }
-
-  findTodayBooking(day) {
-    return this.bookings.find(booking => booking.date === day);
-  }
 }
 
   module.exports = Customer;
