c1113cc3db8eb3c8c6e794b25fdb0ec305facc45
Hannah Hudson <hannah.hch@gmail.com>
2020-06-01 10:16:26 -0600

    removed jQuery from eslint file and added a sweet gradient

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
-// An example of how you import jQuery into a JS file if you use jQuery in that file
-import $ from 'jquery';
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
