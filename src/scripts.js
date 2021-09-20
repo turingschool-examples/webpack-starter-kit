995f75e0a045881f1f3d7578695fbebe8e5f7d41
Hannah Hudson <hannah.hch@gmail.com>
2021-09-20 13:56:07 -0600

    Add scss example

--- src/scripts.js
+++ src/scripts.js
@@ -7,4 +7,5 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+
 console.log('This is the JavaScript entry file - your code begins here.');
