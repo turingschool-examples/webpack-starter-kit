3c5a806e27b0e45ad09a25a1b627dad806cc9aa4
Grace Gardner <graceirisgardner@gmail.com>
2022-01-11 10:31:02 -0700

    Initial commit

--- src/scripts.js
+++ src/scripts.js
@@ -1,6 +1,8 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
 
+// initial commit
+
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
