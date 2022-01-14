f41e5c7b814d64cda2865307279fb80db1e2bf79
Grace Gardner <graceirisgardner@gmail.com>
2022-01-14 14:47:58 -0700

    Make test data sets

---
+++ test/user-data.js
@@ -0,0 +1,46 @@
+
+module.exports = [
+    {
+        "id": 1,
+        "name": "Leatha Ullrich"
+    },
+    {
+        "id": 2,
+        "name": "Rocio Schuster"
+    },
+    {
+        "id": 3,
+        "name": "Kelvin Schiller"
+    },
+    {
+        "id": 4,
+        "name": "Kennedi Emard"
+    },
+    {
+        "id": 5,
+        "name": "Rhiannon Little"
+    },
+    {
+        "id": 6,
+        "name": "Fleta Schuppe"
+    },
+    {
+        "id": 7,
+        "name": "Dell Rath"
+    },
+    {
+        "id": 8,
+        "name": "Era Hand"
+    },
+    {
+        "id": 9,
+        "name": "Faustino Quitzon"
+    },
+    {
+        "id": 10,
+        "name": "Tony Armstrong"
+    },
+    {
+        "id": 11,
+        "name": "Melissa Sauer"
+    }]
