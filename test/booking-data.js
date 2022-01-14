c5f599375080718c1b363c565db920b72b0e14f1
Grace Gardner <graceirisgardner@gmail.com>
2022-01-14 14:36:21 -0700

    Setup file structure

---
+++ test/booking-data.js
@@ -0,0 +1,37 @@
+
+module.exports = [
+    {
+        "id": "5fwrgu4i7k55hl6sz",
+        "userID": 9,
+        "date": "2022/04/22",
+        "roomNumber": 15,
+        "roomServiceCharges": []
+    },
+    {
+        "id": "5fwrgu4i7k55hl6t5",
+        "userID": 43,
+        "date": "2022/01/24",
+        "roomNumber": 24,
+        "roomServiceCharges": []
+    },
+    {
+        "id": "5fwrgu4i7k55hl6t6",
+        "userID": 13,
+        "date": "2022/01/10",
+        "roomNumber": 12,
+        "roomServiceCharges": []
+    },
+    {
+        "id": "5fwrgu4i7k55hl6t7",
+        "userID": 20,
+        "date": "2022/02/16",
+        "roomNumber": 7,
+        "roomServiceCharges": []
+    },
+    {
+        "id": "5fwrgu4i7k55hl6t8",
+        "userID": 1,
+        "date": "2022/02/05",
+        "roomNumber": 12,
+        "roomServiceCharges": []
+    }]
