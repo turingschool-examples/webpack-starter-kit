e46182f783ea0bba50b35343042e73dfa043f2b6
Grace Gardner <graceirisgardner@gmail.com>
2022-01-15 13:31:09 -0700

    Add booking and user class

--- test/rooms-test.js
+++ test/rooms-test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 const expect = chai.expect;
 import rooms from "./rooms-data.js";
+import Room from "../src/classes/rooms-class.js"
 
 
 
@@ -9,7 +10,7 @@ describe('Room', () => {
 
     beforeEach(() => {
         roomOne = new Room(rooms[0])
-    })
+    });
 
     it('Should be a function', () => {
         expect(Room).to.be.a('function');
@@ -17,26 +18,26 @@ describe('Room', () => {
 
     it('Should have a number', () => {
         expect(roomOne.number).to.equal(1);
-    })
+    });
 
     it('Should have a type', () => {
         expect(roomOne.roomType).to.equal("residential suite");
-    })
+    });
 
     it('Should be able to have a bidet', () => {
         expect(roomOne.bidet).to.equal(true);
-    })
+    });
 
     it('Should have a bed size', () => {
         expect(roomOne.bedSize).to.equal("queen");
-    })
+    });
 
     it('Should have a number of beds', () => {
         expect(roomOne.numBeds).to.equal(1);
-    })
+    });
 
     it('Should have a nightly cost', () => {
         expect(roomOne.costPerNight).to.equal(358.4)
-    })
-})
+    });
+});
 
