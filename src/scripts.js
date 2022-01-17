78eb96b82e926c1b919673ef627e4f3804cb29a3
Grace Gardner <graceirisgardner@gmail.com>
2022-01-16 17:10:57 -0700

    Add initial promise for apis

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,13 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// initial commit
-
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import {customersData, userData, roomsData, bookingsData, postBooking} from './apiCalls';
+import Hotel from './classes/hotel-class';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+let hotel;
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
+const loadPage = () => {
+  Promise.all([customersData, userData, roomsData, bookingsData])
+    .then(data => {
+      hotel = new Hotel(data[3], data[2], data[0])
+    })
+}
