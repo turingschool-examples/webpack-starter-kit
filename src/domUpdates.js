810da6b1974693f91ee3c86b8a1b666e1610bacc
Grace Gardner <graceirisgardner@gmail.com>
2022-01-17 13:09:18 -0700

    Display rooms available

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,6 +9,19 @@ const userBookings = document.getElementById('userBookings');
 const userTotal = document.getElementById('userTotal');
 const bookingButton = document.getElementById('bookingButton');
 const bookingSection = document.getElementById('bookingSection');
+const date = document.getElementById('date');
+const singleRoom = document.getElementById('singleRoom');
+const juniorSuite = document.getElementById('juniorSuite');
+const suite = document.getElementById('suite');
+const residentialSuite = document.getElementById('residentialSuite');
+const checkAvailabilityButton = document.getElementById('checkAvailability');
+const availabilityForDate = document.getElementById('availabilityForDate');
+const fierceApology = document.getElementById('fierceApology');
+const roomDisplay = document.getElementById('roomDisplay');
+const bookingForm = document.getElementById('bookingForm');
+
+let selectedDate;
+let selectedRoomTypes = [];
 
 const hide = (toHide) => {
 toHide.forEach(element => {
@@ -44,6 +57,43 @@ const displayTotal = () => {
   userTotal.innerText = `Total Spent with Us: $${hotel.currentCustomer.total}`
 };
 
+const assignSelectedData = () => {
+  selectedDate = date.value;
+  let roomTypes = [singleRoom, juniorSuite, suite, residentialSuite]
+  selectedRoomTypes = roomTypes.reduce((acc, type) => {
+    if(type.checked){
+    acc.push(type.value);
+    }
+    return acc
+  }, [])
+};
+
+const checkRoomAvailability = () => {
+  event.preventDefault()
+  assignSelectedData()
+  if(selectedDate && selectedRoomTypes.length > 0){
+    displayRooms()
+    showHide([roomDisplay], [bookingForm, fierceApology])
+  } else {
+    show([fierceApology, bookingForm])
+  }
+}
+
+const displayRooms = () => {
+  hotel.filterRooms(selectedRoomTypes, selectedDate)
+  availabilityForDate.innerText = `Rooms available for ${hotel.selectedDate}`
+  roomAvailability.innerHTML = hotel.availableRooms.reduce((acc, room) => {
+    acc += `<div class="available-rooms">
+              <h3>Type of room: ${room.roomType}</h3>
+              <p>Beds: ${room.numBeds} ${room.bedSize}</p>
+              <p>Bidet: ${room.bidet}</p>
+              <p>Room ${room.number} is ${room.costPerNight} per night</p>
+              <button class="book-button" value="${room.number}">Book Room</button>
+            </div>`
+    return acc
+  }, '')
+}
+
 let domUpdates = {
 
 
@@ -67,6 +117,7 @@ showAvailableBookings() {
 
 profileButton.addEventListener('click', domUpdates.showProfile);
 bookingButton.addEventListener('click', domUpdates.showAvailableBookings);
+checkAvailabilityButton.addEventListener('click', checkRoomAvailability)
 
 
 
