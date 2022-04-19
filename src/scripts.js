1528eb68a755ada4551e1e37ea9908200765e77c
Heather Faerber <heather@turing.com>
2022-04-19 10:10:32 -0600

    Remove sass files and add css file

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
