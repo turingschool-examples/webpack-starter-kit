6fb0ae373a018724e8f14810f25062db59119729
Emily Miles <ms.emily.j.miles@gmail.com>
2022-09-23 17:57:32 -0400

    Refactor tests to match new property names

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -28,27 +28,27 @@ describe('Destination', () => {
   it('should take in all the destination data for all trips', () => {
     expect(destination1).to.deep.equal({
       id: 1,
-      destination: 'Lima, Peru',
-      estimatedLodgingCostPerDay: 70,
-      estimatedFlightCostPerPerson: 400,
-      image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
-      alt: 'overview of city buildings with a clear sky'
+      location: 'Lima, Peru',
+      lodgingCost: 70,
+      flightCost: 400,
+      locationImage: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+      imageAltText: 'overview of city buildings with a clear sky'
     });
     expect(destination2).to.deep.equal({
       id: 6,
-      destination: 'Jakarta, Indonesia',
-      estimatedLodgingCostPerDay: 70,
-      estimatedFlightCostPerPerson: 890,
-      image: 'https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-      alt: 'lit up city at night'
+      location: 'Jakarta, Indonesia',
+      lodgingCost: 70,
+      flightCost: 890,
+      locationImage: 'https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      imageAltText: 'lit up city at night'
     });
     expect(destination3).to.deep.equal({
       id: 11,
-      destination: 'Mikonos, Greece',
-      estimatedLodgingCostPerDay: 140,
-      estimatedFlightCostPerPerson: 1000,
-      image: 'https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80',
-      alt: 'cityscape along the water during the day'
+      location: 'Mikonos, Greece',
+      lodgingCost: 140,
+      flightCost: 1000,
+      locationImage: 'https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80',
+      imageAltText: 'cityscape along the water during the day'
     });
   });
 
@@ -59,32 +59,32 @@ describe('Destination', () => {
   });
 
   it('should have a destination\'s location', () => {
-    expect(destination1.destination).to.equal('Lima, Peru');
-    expect(destination2.destination).to.equal('Jakarta, Indonesia');
-    expect(destination3.destination).to.equal('Mikonos, Greece');
+    expect(destination1.location).to.equal('Lima, Peru');
+    expect(destination2.location).to.equal('Jakarta, Indonesia');
+    expect(destination3.location).to.equal('Mikonos, Greece');
   });
 
   it('should have a destination\'s estimated lodging cost per day', () => {
-    expect(destination1.estimatedLodgingCostPerDay).to.equal(70);
-    expect(destination2.estimatedLodgingCostPerDay).to.equal(70);
-    expect(destination3.estimatedLodgingCostPerDay).to.equal(140);
+    expect(destination1.lodgingCost).to.equal(70);
+    expect(destination2.lodgingCost).to.equal(70);
+    expect(destination3.lodgingCost).to.equal(140);
   });
 
   it('should have a destination\'s estimated flight cost per person', () => {
-    expect(destination1.estimatedFlightCostPerPerson).to.equal(400);
-    expect(destination2.estimatedFlightCostPerPerson).to.equal(890);
-    expect(destination3.estimatedFlightCostPerPerson).to.equal(1000);
+    expect(destination1.flightCost).to.equal(400);
+    expect(destination2.flightCost).to.equal(890);
+    expect(destination3.flightCost).to.equal(1000);
   });
 
   it('should have a destination\'s location image', () => {
-    expect(destination1.image).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80');
-    expect(destination2.image).to.equal('https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
-    expect(destination3.image).to.equal('https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80');
+    expect(destination1.locationImage).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80');
+    expect(destination2.locationImage).to.equal('https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+    expect(destination3.locationImage).to.equal('https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80');
   });
 
   it('should have a destination\'s location image alternative text', () => {
-    expect(destination1.alt).to.equal('overview of city buildings with a clear sky');
-    expect(destination2.alt).to.equal('lit up city at night');
-    expect(destination3.alt).to.equal('cityscape along the water during the day');
+    expect(destination1.imageAltText).to.equal('overview of city buildings with a clear sky');
+    expect(destination2.imageAltText).to.equal('lit up city at night');
+    expect(destination3.imageAltText).to.equal('cityscape along the water during the day');
   });
 });
