ef53b82318822c103e2a48949637b98316a3e0ea
Emily Miles <ms.emily.j.miles@gmail.com>
2022-09-22 22:37:53 -0400

    Add tests for constructor properties

--- test/Destination-test.js
+++ test/Destination-test.js
@@ -2,12 +2,17 @@ import chai from 'chai';
 const expect = chai.expect;
 
 import Destination from '../src/classes/Destination.js';
+import destinationData from '../data/sample-destinationData.js'
 
 describe('Destination', () => {
-  let destination;
+  let destination1;
+  let destination2;
+  let destination3;
 
   beforeEach( () => {
-    destination = new Destination();
+    destination1 = new Destination(destinationData[0]);
+    destination2 = new Destination(destinationData[5]);
+    destination3 = new Destination(destinationData[10]);
   });
 
   it('should be a function', () => {
@@ -15,6 +20,71 @@ describe('Destination', () => {
   });
 
   it('should be an instance of Destination', () => {
-    expect(destination).to.be.an.instanceOf(Destination);
+    expect(destination1).to.be.an.instanceOf(Destination);
+    expect(destination2).to.be.an.instanceOf(Destination);
+    expect(destination3).to.be.an.instanceOf(Destination);
+  });
+
+  it('should take in all the destination data for all trips', () => {
+    expect(destination1).to.deep.equal({
+      id: 1,
+      destination: 'Lima, Peru',
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 400,
+      image: 'https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+      alt: 'overview of city buildings with a clear sky'
+    });
+    expect(destination2).to.deep.equal({
+      id: 6,
+      destination: 'Jakarta, Indonesia',
+      estimatedLodgingCostPerDay: 70,
+      estimatedFlightCostPerPerson: 890,
+      image: 'https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+      alt: 'lit up city at night'
+    });
+    expect(destination3).to.deep.equal({
+      id: 11,
+      destination: 'Mikonos, Greece',
+      estimatedLodgingCostPerDay: 140,
+      estimatedFlightCostPerPerson: 1000,
+      image: 'https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80',
+      alt: 'cityscape along the water during the day'
+    });
+  });
+
+  it('should have a destination\'s id', () => {
+    expect(destination1.id).to.equal(1);
+    expect(destination2.id).to.equal(6);
+    expect(destination3.id).to.equal(11);
+  });
+
+  it('should have a destination\'s location', () => {
+    expect(destination1.destination).to.equal('Lima, Peru');
+    expect(destination2.destination).to.equal('Jakarta, Indonesia');
+    expect(destination3.destination).to.equal('Mikonos, Greece');
+  });
+
+  it('should have a destination\'s estimated lodging cost per day', () => {
+    expect(destination1.estimatedLodgingCostPerDay).to.equal(70);
+    expect(destination2.estimatedLodgingCostPerDay).to.equal(70);
+    expect(destination3.estimatedLodgingCostPerDay).to.equal(140);
+  });
+
+  it('should have a destination\'s estimated flight cost per person', () => {
+    expect(destination1.estimatedFlightCostPerPerson).to.equal(400);
+    expect(destination2.estimatedFlightCostPerPerson).to.equal(890);
+    expect(destination3.estimatedFlightCostPerPerson).to.equal(1000);
+  });
+
+  it('should have a destination\'s location image', () => {
+    expect(destination1.image).to.equal('https://images.unsplash.com/photo-1489171084589-9b5031ebcf9b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80');
+    expect(destination2.image).to.equal('https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80');
+    expect(destination3.image).to.equal('https://images.unsplash.com/photo-1573783309724-e44b859f5a85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1953&q=80');
+  });
+
+  it('should have a destination\'s location image alternative text', () => {
+    expect(destination1.alt).to.equal('overview of city buildings with a clear sky');
+    expect(destination2.alt).to.equal('lit up city at night');
+    expect(destination3.alt).to.equal('cityscape along the water during the day');
   });
 });
