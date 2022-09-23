3ac6e89c70725ec249ce1129ef3d7d33ebf08471
Emily Miles <ms.emily.j.miles@gmail.com>
2022-09-23 17:56:44 -0400

    Refactor property names

--- src/classes/Destination.js
+++ src/classes/Destination.js
@@ -1,11 +1,11 @@
 class Destination {
   constructor(destinationData) {
     this.id = destinationData.id;
-    this.destination = destinationData.destination;
-    this.estimatedLodgingCostPerDay = destinationData.estimatedLodgingCostPerDay;
-    this.estimatedFlightCostPerPerson = destinationData.estimatedFlightCostPerPerson;
-    this.image = destinationData.image;
-    this.alt = destinationData.alt;
+    this.location = destinationData.destination;
+    this.lodgingCost = destinationData.estimatedLodgingCostPerDay;
+    this.flightCost = destinationData.estimatedFlightCostPerPerson;
+    this.locationImage = destinationData.image;
+    this.imageAltText = destinationData.alt;
   };
 };
 
