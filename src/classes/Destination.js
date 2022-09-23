135c66a373dbc0497462e385bd2f34dddfef14b1
Emily Miles <ms.emily.j.miles@gmail.com>
2022-09-22 22:37:06 -0400

    Add constructor properties for Destination class

--- src/classes/Destination.js
+++ src/classes/Destination.js
@@ -1,7 +1,12 @@
 class Destination {
-  constructor() {
-
-  }
-}
+  constructor(destinationData) {
+    this.id = destinationData.id;
+    this.destination = destinationData.destination;
+    this.estimatedLodgingCostPerDay = destinationData.estimatedLodgingCostPerDay;
+    this.estimatedFlightCostPerPerson = destinationData.estimatedFlightCostPerPerson;
+    this.image = destinationData.image;
+    this.alt = destinationData.alt;
+  };
+};
 
 export default Destination;
