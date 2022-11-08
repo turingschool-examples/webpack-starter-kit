455c2f2c8274b94910d633e0d2ccc83dc66314bf
Dani Bagley <bagley.a.danielle@gmail.com>
2022-11-08 13:31:28 -0700

    Initial commit

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,9 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+import "./css/styles.css";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import "./images/turing-logo.png";
 
-
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log("This is the JavaScript entry file - your code begins here.");
