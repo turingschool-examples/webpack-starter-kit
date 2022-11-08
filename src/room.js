7cdede7a693ceeb8f76e5254ad76b0628bac12d0
Dani Bagley <bagley.a.danielle@gmail.com>
2022-11-08 13:50:11 -0700

    Add room class and constructor

---
+++ src/room.js
@@ -0,0 +1,10 @@
+class Room {
+  constructor(data) {
+    this.number = data.number;
+    this.roomType = data.roomType;
+    this.bidet = data.bidet;
+    this.bedSize = data.bidet;
+    this.numBeds = data.numBeds;
+    this.costPerNight = data.costPerNight;
+  }
+}
