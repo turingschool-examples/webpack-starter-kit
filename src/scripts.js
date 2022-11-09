6e996891945148318c09c3fe2f500884004c9aab
Colin Ciervo <colinciervo@@gmail.com>
2022-11-09 15:02:47 -0500

    Add functionality to display bookings

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,8 @@ import Customer from './classes/customer'
 
 
 //Query Selectors
+const bookingsList = document.querySelector(".bookings-list")
+
 
 //Variables
 let bookings
@@ -13,37 +15,42 @@ let rooms
 let customers
 let singleCustomer
 let currentCustomer
+let customersRooms = []
 
 //Functions
 const fetchApiCalls = () => {
     apiCalls.fetchData().then(data => {
-      bookings = data[0].bookings
-      rooms = data[1].rooms
-      customers = data[2].customers
-      singleCustomer = data[3]
+        bookings = data[0].bookings
+        rooms = data[1].rooms
+        customers = data[2].customers
+        singleCustomer = data[3]
 
-      loadHandler()
+        loadHandler()
     })
-  }
+}
 
-  function loadHandler() {
+function loadHandler() {
     console.log(bookings, rooms, customers, singleCustomer)
     currentCustomer = new Customer(singleCustomer, bookings)
-    console.log(currentCustomer)
     totalBookingsCost()
-    console.log(totalBookingsCost())
-  }
+    displayUserData()
+}
 
-  function totalBookingsCost() {
-    const customersRooms = []
+function totalBookingsCost() {
     currentCustomer.findAllBookings()
     currentCustomer.customersBookings.forEach(booking => {
         customersRooms.push(rooms.filter(room => room.number === booking.roomNumber)[0])
     })
     return customersRooms.reduce((acc, cur) => {
-     return cur.costPerNight + acc
-    },0)
-  }
+        return cur.costPerNight + acc
+    }, 0)
+}
+
+function displayUserData() {
+    currentCustomer.customersBookings.forEach(booking => {
+        bookingsList.insertAdjacentHTML('beforeend' ,`<p>${booking.date} Room Number:${booking.roomNumber}</p>`)
+    })
+}
 
 //Event Listeners
 addEventListener('load', fetchApiCalls())
