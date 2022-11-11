5d0ee5963eb51f26924f4b5fea2c81740070696c
Dani Bagley <bagley.a.danielle@gmail.com>
2022-11-11 12:51:06 -0700

    add room population function

--- src/scripts.js
+++ src/scripts.js
@@ -3,8 +3,38 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import "./css/styles.scss";
+import roomData from "./sampleData/room_sample_data";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import "./images/turing-logo.png";
+import "./images/HotelRoom1.png";
+import "./images/HotelRoom2.png";
+import "./images/HotelRoom3.png";
+import "./images/HotelRoom4.png";
 
 console.log("This is the JavaScript entry file - your code begins here.");
+
+//VARIABLES
+
+//QUERY SELECTORS
+let availableRooms = document.querySelector(".room-thumbnails");
+
+//FETCH
+
+function displayAvailableRooms(data) {
+  data.forEach((room) => {
+    availableRooms.innerHTML += `
+    <section class="single-room-thumbnail" id ="${room.number}"> 
+      <img class="single-room-img" src="./images/HotelRoom4.png" alt="Image of room ${room.number}"> 
+        <div class="room-info"> 
+          <p>Room number: ${room.number}</p>
+          <p>Room type: ${room.roomType}</p>
+          <p>Bidet: ${room.bidet}</p>
+          <p>Bed size: ${room.bedSize}</p>
+          <p>Number of beds: ${room.numBeds}</p>
+          <p>Cost per night: ${room.costPerNight}</p>
+        </div> 
+    </section>`;
+  });
+}
+
+displayAvailableRooms(roomData);
