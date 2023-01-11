2a65ce031e4b3cb71b38c1db1b168b4e1919b931
Adam Hughes <adamjordanhughes@gmail.com>
2023-01-10 21:58:33 -0500

    finish setting up the window load logic

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,28 @@
+// import live here
 import './css/styles.css';
 
 import './images/turing-logo.png'
 
+// query selectors go here
+
+// global variables used for data model here
+let travelersRepo 
+let currentTraveler
+
+//functions here
+function apiCalls () {
+    getApiData()
+    loadHandler()
+}
+
+function getApiData() {
+    let getAllTravelers = fetch('http://localhost:3001/api/v1/travelers	')
+}
+
+
+function loadHandler() {
+    console.log("this loaded")
+}
+
+//event listeners here
+window.addEventListener("load", apiCalls())
