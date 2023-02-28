79e66a90ca8e65ee2920ff2dced8f76220011dc4
trevorfitz0 <trevorfitz0@gmail.com>
2023-02-28 16:57:35 -0700

    Promise all sends data to a Hotel Data class

--- src/scripts.js
+++ src/scripts.js
@@ -16,3 +16,15 @@ const rooms =  fetch('http://localhost:3001/api/v1/rooms')
 const bookings = fetch('http://localhost:3001/api/v1/bookings')
   .then((res) => res.json())
 
+  Promise.all([customers, rooms, bookings])
+  .then((data) => {
+    let allData = {
+      customers: data[0].customers,
+      rooms: data[1].rooms,
+      bookings: data[2].bookings
+    }
+    return allData
+  })
+  .then((allData) => {
+    hotelData = new HotelData(allData)
+  })
