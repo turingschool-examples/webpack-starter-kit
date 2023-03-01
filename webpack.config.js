9bdf3d2012aa6c2268c5a5bb91aeef523ca7e90e
Kirk Hauck <kirkihauck@gmail.com>
2023-02-28 18:58:53 -0700

    feat: Add images and adjust webpack to accept jpeg

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         ]
     },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif|jpeg)$/,
         use: [
           {
             loader: 'file-loader',
