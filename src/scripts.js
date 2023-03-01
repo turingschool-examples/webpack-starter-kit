e6ccdd556b2e2926e493d60d380feb6c675b9489
Kirk Hauck <kirkihauck@gmail.com>
2023-03-01 14:00:22 -0700

    feat: Add remaining options to room filter

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,6 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+// IMPORTS
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/junior-suite.jpeg';
 import './images/presidential-suite.jpeg';
 import './images/single-room.jpeg';
 import './images/suite.jpeg';
-
-
-console.log('This is the JavaScript entry file - your code begins here.');
