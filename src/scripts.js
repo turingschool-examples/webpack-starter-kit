dd614400af45819b5f82f4d7b3fb62cda07b90ad
Kirk Hauck <kirkihauck@gmail.com>
2023-03-02 09:25:56 -0700

    bug: Return correct bookings for some dates but not others

--- src/scripts.js
+++ src/scripts.js
@@ -37,15 +37,33 @@ window.addEventListener('load', () => {
       loginCustomer();
       showBookingTotal();
       showCustomerBookings();
+      console.log(getVacancies('2023/01/11'))
     }
   );
 });
 
 searchButton.addEventListener('click', (event) => {
   event.preventDefault();
+  const date = dateInput.value;
+  // console.log(getVacancies(date));
+  getVacancies(date);
 });
 
 // FUNCTIONS
+
+const getVacancies = date => {
+  date = date.replace(/\-/g, '/');
+  const daysBookings = bookings.filter(booking => booking.date === date);
+  // console.log(daysBookings)
+  const vacancies = rooms.filter(room => daysBookings.find(booking => {
+    console.log('booking', booking.roomNumber);
+    console.log('room', room.number);
+    return booking.roomNumber !== room.number
+  }));
+  // Working on weird issue where some days work but others don't
+  return vacancies;
+}
+
 
 const showBookingTotal = () => {
   const customerBookings = bookings.filter(booking => booking.userID === customer.id);
@@ -56,13 +74,14 @@ const showBookingTotal = () => {
   }, 0)
   
   total = total.toFixed(2);
-
+  
   totalBookings.innerText = `You have ${customerBookings.length} bookings for a total of $${total}`
 }
 
 const showCustomerBookings = () => {
   const customerBookings = bookings.filter(booking => booking.userID === customer.id);
   customerBookings.forEach(booking => {
+    const bookingDate = arrangeDate(booking.date);
     const room = rooms.find(room => room.number === booking.roomNumber);
     const image = room.roomType.replace(' ', '-');
     const roomName = room.roomType.split(' ').map(word => word[0].toUpperCase() + word.substring(1)).join(' ');
@@ -73,31 +92,43 @@ const showCustomerBookings = () => {
     } else {
       bidetStatus = 'Does not Include Bidet'
     }
-
+    
     roomsDisplay.innerHTML += `
-      <figure>
-        <img src="./images/${image}.jpeg" alt="image of ${room.roomType}">
-        <figcaption>
-          <div>
-            <h4>Room #${booking.roomNumber} - ${roomName}</h4>
-            <h5>$${room.costPerNight}</h5>
-          </div>
-          <div class="room-description">
-            <p>Bed Size: ${bedSize}</p>
-            <p>Number of Beds: 2</p>
-            <p>${bidetStatus}</p>
-          </div>
-          <div>  
-            <p class="booked">Booked for ${booking.date}</p>
-          </div>
-        </figcaption>
-      </figure>
+    <figure>
+    <img src="./images/${image}.jpeg" alt="image of ${room.roomType}">
+    <figcaption>
+    <div>
+    <h4>Room #${booking.roomNumber} - ${roomName}</h4>
+    <h5>$${room.costPerNight}</h5>
+    </div>
+    <div class="room-description">
+    <p>Bed Size: ${bedSize}</p>
+    <p>Number of Beds: 2</p>
+    <p>${bidetStatus}</p>
+    </div>
+    <div>  
+    <p class="booked">Booked for ${bookingDate}</p>
+    </div>
+    </figcaption>
+    </figure>
     `;
   });
 }
 
 // Will need to adjust to accept customer login
 const loginCustomer = () => customer = customers[0];
+
+function arrangeDate(date) {
+  const monthAndDay = date.substring(5);
+  const year = date.substring(0, 4);
+  return monthAndDay + '/' + year
+}
+
+// function arrangeYearToDay(date) {
+//   const monthAndDay = date.substring(0, 5);
+//   const year = date.substring(6);
+//   return year + '/' + monthAndDay;
+// }
 
 const handleError = (error) => roomsDisplay.innerText = `${error}, sorry!`;
 
