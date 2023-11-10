8feef8309402ea0664fa96729b13edf07a385ae3
Zen McMillan <zenmcmillan@gmail.com>
2023-11-09 18:09:01 -0600

    Create apiCalls file for the network requests and create two fetch requests functions from the travelers endpoint and destinations endpoint respectively

---
+++ src/apiCalls.js
@@ -0,0 +1,33 @@
+export const fetchTavelers = () => {
+  fetch("http://localhost:3001/api/v1/travelers")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      } 
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+  };
+
+export const fetchDestinations = () => {
+  fetch("http://localhost:3001/api/v1/destinations")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+  }; 
