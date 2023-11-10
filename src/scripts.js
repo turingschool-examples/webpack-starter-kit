217cdd624e2700facad1b0c8464c5c7559e5b57b
Zen McMillan <zenmcmillan@gmail.com>
2023-11-10 10:44:48 -0600

    Add annual total section and buttons and give it clicking functionality

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import { fetchTavelers, fetchDestinations } from './apiCalls';
 
 console.log('This is the JavaScript entry file - your code begins here.');
 
@@ -14,11 +14,14 @@ const upcomingTripsSection = document.querySelector('.upcoming-trips-section');
 const pendingTripsSection = document.querySelector('.pending-trips-section');
 const pastTripsSection = document.querySelector('.past-trips-section');
 const bookATripSection = document.querySelector('.book-a-trip-section');
+const annualTotalSection = document.querySelector('.annual-total-section');
 
 const upcomingTripsButton = document.querySelector('.upcoming-trips-button');
 const pendingTripsButton = document.querySelector('.pending-trips-button');
 const pastTripsButton = document.querySelector('.past-trips-button');
+const annualTotalButton = document.querySelector('.annual-total-button')
 const bookATripButton = document.querySelector('.book-a-trip-button')
+
   
 window.addEventListener('load', () => {
   showUpcomingTrips();
@@ -36,14 +39,27 @@ pastTripsButton.addEventListener('click', () => {
   showPastTrips()
 })
 
+annualTotalButton.addEventListener('click', () => {
+  showAnnualCostSection()
+})
+
 bookATripButton.addEventListener('click', () => {
   showBookATripSection()
 })
+
+const showAnnualCostSection = () => {
+  upcomingTripsSection.classList.add("hidden");
+  pendingTripsSection.classList.add("hidden");
+  pastTripsSection.classList.add("hidden");
+  bookATripSection.classList.add("hidden");
+  annualTotalSection.classList.remove("hidden");
+}
 
 const showBookATripSection = () => {
   upcomingTripsSection.classList.add('hidden');
   pendingTripsSection.classList.add('hidden');
   pastTripsSection.classList.add("hidden");
+  annualTotalSection.classList.add("hidden");
   bookATripSection.classList.remove('hidden')
 }
 
@@ -51,6 +67,7 @@ const showPastTrips = () => {
   upcomingTripsSection.classList.add('hidden');
   pendingTripsSection.classList.add('hidden');
   bookATripSection.classList.add("hidden");
+  annualTotalSection.classList.add("hidden");
   pastTripsSection.classList.remove('hidden');
 }
 
@@ -58,6 +75,7 @@ const showPendingTrips = () => {
   upcomingTripsSection.classList.add('hidden');
   pastTripsSection.classList.add('hidden');
   bookATripSection.classList.add('hidden');
+  annualTotalSection.classList.add("hidden");
   pendingTripsSection.classList.remove('hidden');
 }
 
@@ -65,6 +83,7 @@ const showUpcomingTrips = () => {
   pendingTripsSection.classList.add('hidden');
   pastTripsSection.classList.add('hidden');
   bookATripSection.classList.add('hidden');
+  annualTotalSection.classList.add("hidden");
   upcomingTripsSection.classList.remove('hidden');
 }
 
