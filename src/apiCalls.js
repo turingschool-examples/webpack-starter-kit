ffcd92c4987f9cef15cd83d9c1013db4f26566b6
Zen McMillan <zenmcmillan@gmail.com>
2023-11-10 19:28:30 -0600

    Make a fetch call for the user

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -31,3 +31,20 @@ export const fetchDestinations = () => {
       console.error("Fetch error:", err);
     });
   }; 
+
+export const fetchLoginInfo = (userInfo) => {
+  let endpoint = userInfo.endpoint;
+  fetch(endpoint)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.error("Fetch error:", err);
+    });
+};
