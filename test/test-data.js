bfe76f0bc7578845d8f9913f0c5859451d07bff7
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-04-20 13:59:50 -0600

    feat: Will add test data files.

---
+++ test/test-data.js
@@ -0,0 +1,163 @@
+const rooms = [
+    {
+      "number": 1,
+      "roomType": "residential suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 358.4
+    },
+    {
+      "number": 2,
+      "roomType": "suite",
+      "bidet": false,
+      "bedSize": "full",
+      "numBeds": 2,
+      "costPerNight": 477.38
+    },
+    {
+      "number": 3,
+      "roomType": "single room",
+      "bidet": false,
+      "bedSize": "king",
+      "numBeds": 1,
+      "costPerNight": 491.14
+    },
+    {
+      "number": 4,
+      "roomType": "single room",
+      "bidet": false,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 429.44
+    },
+    {
+      "number": 5,
+      "roomType": "single room",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 2,
+      "costPerNight": 340.17
+    },
+    {
+      "number": 6,
+      "roomType": "junior suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 397.02
+    }
+]
+const bookings = [
+    {
+      "id": "5fwrgu4i7k55hl6sz",
+      "userID": 9,
+      "date": "2022/04/22",
+      "roomNumber": 15,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t5",
+      "userID": 9,
+      "date": "2022/01/24",
+      "roomNumber": 24,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t6",
+      "userID": 13,
+      "date": "2022/01/10",
+      "roomNumber": 12,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t7",
+      "userID": 20,
+      "date": "2022/02/16",
+      "roomNumber": 7,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t8",
+      "userID": 1,
+      "date": "2022/02/05",
+      "roomNumber": 12,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t9",
+      "userID": 9,
+      "date": "2023/12/14",
+      "roomNumber": 14,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6ta",
+      "userID": 25,
+      "date": "2022/01/11",
+      "roomNumber": 9,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tb",
+      "userID": 49,
+      "date": "2022/02/06",
+      "roomNumber": 5,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tc",
+      "userID": 22,
+      "date": "2023/11/30",
+      "roomNumber": 13,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6td",
+      "userID": 27,
+      "date": "2022/01/31",
+      "roomNumber": 20,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6te",
+      "userID": 44,
+      "date": "2022/01/19",
+      "roomNumber": 8,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tf",
+      "userID": 36,
+      "date": "2022/01/25",
+      "roomNumber": 2,
+  
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tg",
+      "userID": 34,
+      "date": "2022/02/03",
+      "roomNumber": 17,
+  
+    }
+]
+const sampleUsers = [
+    {
+        "id": 1,
+        "name": "Rhiannon Little"
+      },
+      {
+        "id": 9,
+        "name": "Fleta Schuppe"
+      },
+      {
+        "id": 7,
+        "name": "Dell Rath"
+      }
+]
+
+export {
+    rooms,
+    bookings,
+    sampleUsers
+}
