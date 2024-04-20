8f3a09e8b63476b9d8953c34e87e437da9973a0e
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-04-20 14:33:43 -0600

    feat: Will add tests for calculateTotalCost().

--- test/tests.js
+++ test/tests.js
@@ -25,7 +25,7 @@ describe('userBookings()',()=>{
         "id": "5fwrgu4i7k55hl6t8",
         "userID": 1,
         "date": "2022/02/05",
-        "roomNumber": 12,
+        "roomNumber": 5,
     
       }]
     );
@@ -34,21 +34,21 @@ describe('userBookings()',()=>{
         "id": "5fwrgu4i7k55hl6sz",
         "userID": 9,
         "date": "2022/04/22",
-        "roomNumber": 15,
+        "roomNumber": 1,
     
       },
       {
         "id": "5fwrgu4i7k55hl6t5",
         "userID": 9,
         "date": "2022/01/24",
-        "roomNumber": 24,
+        "roomNumber": 1,
     
       },
       {
         "id": "5fwrgu4i7k55hl6t9",
         "userID": 9,
         "date": "2023/12/14",
-        "roomNumber": 14,
+        "roomNumber": 3,
     
       }]
     );
@@ -61,7 +61,28 @@ describe('userBookings()',()=>{
 
 describe('getBookingCost()',()=>{
   it('should take in a room number and array of rooms return an object with the room number and its cost',()=>{
-    const costIndex = getBookingCost(rooms[0],rooms)
-    expect(costIndex).to.deep.equal({room: 1, cost: 358.4})
+    const costIndex = getBookingCost(rooms, rooms[0]);
+    expect(costIndex).to.deep.equal({room: 1, cost: 358.4});
+  });
+  it('should return a default object with a cost of zero',()=>{
+    const costIndex = getBookingCost(rooms);
+    const costIndex_1 = getBookingCost(rooms, 'No bookings found.');
+    expect(costIndex).to.deep.equal({room: null, cost: 0.0});
+    expect(costIndex_1).to.deep.equal({room: null, cost: 0.0});
+  });
+});
+
+describe('calculateTotalCost()',()=>{
+  it ('should take in an array of bookings and return their total cost',()=>{
+    const result = userBookings(sampleUsers[1].id, bookings);
+    const totalCost = calculateTotalCost(result);
+    expect(totalCost).to.equal(1207.94);
+  });
+  it ('should return a default cost of 0.00 under various conditions',()=>{
+    const result = userBookings(sampleUsers[2].id, bookings);
+    const totalCost = calculateTotalCost(result);
+    const totalCost_1 = calculateTotalCost();
+    expect(totalCost).to.equal(0.0);
+    expect(totalCost_1).to.equal(0.0);
   });
 });
