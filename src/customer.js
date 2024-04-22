6500584acbc778902438f94183c94b265848d71d
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-04-22 15:27:43 -0500

    complete testing for customer interactions.

--- src/customer.js
+++ src/customer.js
@@ -1,15 +1,67 @@
-const showBooking = (bookings,id) => {
-    return bookings.find(booking => {
-        return booking.id === id
-        
+const showFutureBooking = (bookings) => {
+    const currentDate = new Date();
+    return bookings.filter(booking => {
+        const bookingDate = new Date(booking.date);
+        return bookingDate > currentDate
     })
 }
-const calculateCostPerNight = () =>{}
+const showPastBookings = (bookings) => {
+    const currentDate = new Date()
+    return bookings.filter(booking => {
+        const bookingDate = new Date(booking.date);
+        return bookingDate < currentDate
+    })
+}
+const calculateCostPerNight = (rooms, roomNumber, days) => {
+    let cost = 0 ;
+    rooms.forEach(room => {
+        if (room.number === roomNumber) {
+            cost += room.costPerNight * days; 
+        }
+    });
+    return cost;
+}
 
-const calculateBookingCost = (bookings, id, days) => {}
+const calculateFutureBookingCosts = (bookings,rooms,days) => {
+    let futureBookings = showFutureBooking(bookings)
+    let totalCost = 0
+    futureBookings.forEach(booking =>{
+        const {roomNumber} = booking
+        const cost = calculateCostPerNight(rooms, roomNumber, days)
+        totalCost += cost            
+    })
+    return totalCost
+}
+
+const calculatePastBookingCosts = (bookings,rooms,days) => {
+    let pastBookings = showPastBookings(bookings)
+    let totalCost = 0
+    pastBookings.forEach(booking =>{
+        const {roomNumber} = booking
+        const cost = calculateCostPerNight(rooms, roomNumber, days)
+        totalCost += cost            
+    })
+    return totalCost  
+}
+
+const calculateAllBookingCosts = (bookings, rooms, days) => {
+    let totalCost = 0
+    bookings.forEach(booking =>{
+        const {roomNumber} = booking
+        const cost = calculateCostPerNight(rooms, roomNumber, days)
+        totalCost += cost            
+    })
+    console.log(totalCost)
+    return totalCost  
+}
+
+
 
 module.exports = {
-    showBooking,
-    calculateCostPerNight
-    //calculateBookingCost   
+    showFutureBooking,
+    showPastBookings,
+    calculateCostPerNight,
+    calculateFutureBookingCosts,
+    calculatePastBookingCosts,
+    calculateAllBookingCosts 
 }
--- test/customer-tests.js
+++ test/customer-tests.js
@@ -1,36 +1,56 @@
 import { expect } from "chai";
 import { users,rooms,bookings } from "./mock-data";
-import {  showBooking, calculateCostPerNight, calculateBookingCost} from "../src/customer";
+import {  showFutureBooking,showPastBookings, calculateCostPerNight, calculateFutureBookingCosts,  calculatePastBookingCosts, calculateAllBookingCosts} from "../src/customer";
 
 describe("Bookings", () => {
-    it ("should show future bookings", () => {
-        const futureBooking = showBooking(bookings,"5fwrgu4i7k55hl6t6")
+    it("should show future bookings", () => {
+        const futureBookings = showFutureBooking(bookings)
         
-        expect(futureBooking).to.deep.equal({id:"5fwrgu4i7k55hl6t6", userID:1, date:"2024/9/25", roomNumber:2})
+        expect(futureBookings).to.deep.equal([{
+            id:"5fwrgu4i7k55hl6sz", 
+            userID:1, 
+            date:"2024/5/25", 
+            roomNumber:1
+        },
+        {
+            id:"5fwrgu4i7k55hl6t6", 
+            userID:1, 
+            date:"2024/9/25", 
+            roomNumber:2}
+    ])
     });
     it("should show past bookings", () => {
-        const futureBooking = showBooking(bookings,"5fwrgu4i7k55hl6t9")
+        const pastBookings = showPastBookings(bookings)
     
-        expect(futureBooking).to.deep.equal({id:"5fwrgu4i7k55hl6t9", userID:1, date:"2024/4/20", roomNumber:3})
+        expect(pastBookings).to.deep.equal([{id:"5fwrgu4i7k55hl6t9", userID:1, date:"2024/4/20", roomNumber:3}])
     });
 });
 describe("Cost", () => {
-    it.skip("should calculate the cost of two nights", () => {
-        const cost = calculateCostPerNight(room,1,2)
+    it("should calculate the cost of two nights", () => {
+        const cost = calculateCostPerNight(rooms,1,2)
 
-        expect(cost).to.equal()
+        expect(cost).to.equal(1300.9)
     });
-    it.skip("should calculate the cost of seven nights", () => {
-        const cost = calculateCostPerNight(room,2,7)
-        expect(cost).to.equal()
+    it("should calculate the cost of seven nights", () => {
+        const cost = calculateCostPerNight(rooms,2,7)
+        expect(cost).to.equal(2648.94)
     });
-    it.skip("should calculate total cost of past bookings", () => {
-        const cost = calculateBookingCost(bookings,)
+    it("should calculate total cost of future bookings", () => {
+        const cost = calculateFutureBookingCosts(bookings,rooms,3)
+        const room1Cost = calculateCostPerNight(rooms,1,3)
+        const room2Cost = calculateCostPerNight(rooms, 2, 3)
+        expect(cost).to.equal(room1Cost + room2Cost)  
     });
-    it.skip("should calculate total cost of future bookings", () => {
-        
+    it("should calculate total cost of past bookings", () => {
+        const cost = calculatePastBookingCosts(bookings,rooms, 4)
+        const roomCost = calculateCostPerNight(rooms,3,4)
+        expect(cost).to.equal(roomCost)
     });
-    it.skip("should calculate total cost of all bookings", () => {
-        
+    it("should calculate total cost of all bookings", () => {
+        const cost = calculateAllBookingCosts(bookings, rooms, 2)
+        const room1Cost = calculateCostPerNight(rooms,1,2)
+        const room2Cost = calculateCostPerNight(rooms, 2,2)
+        const room3Cost = calculateCostPerNight(rooms,3,2)
+        expect(cost).to.equal(room1Cost+room2Cost+room3Cost)
     });
 });
