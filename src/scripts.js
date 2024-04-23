a58fab9f6b52e25d32498b1517563655ee2bd000
Gwyneth Patrick <gwynethpatrick235@gmail.com>
2024-04-22 18:34:35 -0600

    feat: will add a data model and import domUpdates in scripts.

--- src/scripts.js
+++ src/scripts.js
@@ -4,13 +4,14 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import { apiCall, fetchUser } from './apiCalls';
 import './css/styles.scss';
+import './domUpdates'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
-const apiData = apiCall()
-const customer = fetchUser(5)
-console.log(customer.getInformation())
-console.log(apiData.getBookings(),apiData.getRooms())
+export const apiData = apiCall()
+export const dataModel ={
+    user: {}
+}
