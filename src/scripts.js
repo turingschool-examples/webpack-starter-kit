6d08be59639435f2e7db419634b282bfafa3b2a6
Kevin Nelson <km.nelson@outlook.com>
2024-06-09 14:09:53 -0400

    Finished Iteration 1 and the MVP

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,11 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/Travel Photo - Beach from Above.jpg'
+import './images/Travel Photo - Rocky Beach from Above.jpg'
+import './images/Travel Photo - Alt Rocky Beach.jpg'
+import './images/Travel Photo - City on Rocky Coast.jpg'
+
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
